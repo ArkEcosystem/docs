f2751ec2d916a778f64b3c9dd99cf0f61cd8e5a5
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-10 14:54:17 +0200

    fix: links on explorer intro page (#39)

--- docs/explorer/intro.md.blade.php
+++ docs/explorer/intro.md.blade.php
@@ -4,21 +4,26 @@ title: Introduction
 
 # Introduction
 
-
-
 ## Using the Explorer
-To understand the basics of the Explorer and to learn how to utilize the functionality within it, follow the guide below:
-<livewire:page-reference path="/docs/explorer/how-to-use-the-explorer" />
 
+To understand the basics of the Explorer and to learn how to utilize the functionality within it, follow the guide below:
+
+<livewire:page-reference path="/docs/explorer/archived/how-to-use-the-explorer" />
 
 ## Deploy a Blockchain Explorer
+
 Whether it's the ARK Public Network or the ARK Development Network, anyone can deploy their own Explorer. To find out how, follow the guide below:
-<livewire:page-reference path="/docs/explorer/running-your-own-explorer" />
+
+<livewire:page-reference path="/docs/explorer/archived/running-your-own-explorer" />
 
 ## Set Custom Branding
+
 The Explorer is highly customizable making it easy for anyone running their own Explorer to customize it to a style of their choosing. Find out how below:
-<livewire:page-reference path="/docs/explorer/how-to-set-custom-branding" />
+
+<livewire:page-reference path="/docs/explorer/archived/how-to-set-custom-branding" />
 
 ## Add New Transaction Types
+
 ARK Core allows developers to create new transaction types through ARK Logic. Custom transaction types can also be added to your Explorer. Find out how below:
-<livewire:page-reference path="/docs/explorer/how-to-add-custom-transactions" />
+
+<livewire:page-reference path="/docs/explorer/archived/how-to-add-custom-transactions" />
