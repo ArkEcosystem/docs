9639f2fcb58cc54724cde3359eabd342e3fb4fc3
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-10 14:22:50 +0200

    docs(explorer): add development, deployment and docker instructions (#38)

--- docs/explorer/running-your-own-explorer.md.blade.php
+++ docs/explorer/running-your-own-explorer.md.blade.php
@@ -4,48 +4,132 @@ title: How To Deploy Blockchain Explorer?
 
 # How To Deploy Blockchain Explorer?
 
-At ARK.io, we have a [mainnet](https://explorer.ark.io) and [devnet](https://dexplorer.ark.io) explorer running to show the current state of the network and what is happening on it. In addition, there is are official mirrors for the mainnet explorer too, which you can find on different services such as [GitHub pages](https://arkecosystem.github.io/explorer/) and [Netlify](https://ark-explorer.netlify.com/#/).
+Deploying the Explorer requires a few things to guarantee smooth operation. If you don't want to deal with all of the deployment hassle we would recommend to use [Laravel Forge](https://forge.laravel.com/) and [Laravel Envoyer](https://envoyer.io/) or [Laravel Vapor](https://vapor.laravel.com/) if you prefer to go serverless and forget about scaling.
 
-To run your own explorer, or an additional mirror of the ARK explorer, there are a couple of ways to achieve this:
+## Core
 
-### 1. Run Explorer On GitHub Pages
+The recommended setup would be that you run a Core instance behind a Firewall with remote access to your database with a read-only user. This instance should have all public services like APIs and Webhooks disabled to reduce the load on it as much as possible. Its sole job should be to sync data and leave as much resources as possible to PostgreSQL.
 
-Our [explorer mirror](https://arkecosystem.github.io/explorer/) runs on GitHub pages and is simply said a branch that contains the `dist` directory that is generated when you build the explorer. To run the explorer on your own GitHub pages website, you can do one of the following:
+We recommend that this instance has at a bare-minimum 8GB of RAM and 4 CPU Cores. This is the absolute bare-minimum but for smooth operations that are able to handle all of the data aggregation and computations that are performed we recommend at least 16GB of RAM and 8 CPU Cores. The ideal setup would be an instance that has 32GB of RAM and 16 CPU Cores which will leave Explorer and Core with more than enough room for spikes in resource consumption. Also, make sure that you configure your PostgreSQL accordingly to the resources available on your machine. You can use a tool like [PGTune](https://pgtune.leopard.in.ua/#/) to get a better idea of what settings would be appropriate.
 
-#### 1.1. Github With Root Repository
+If you would want to further improve the stability and performance of your setup you could run a separate database server that can only be accessed by Core and Explorer instead of running the database and Core on the same server. Keep in mind that you should keep your Core and Database server in the same geo location to avoid high latency which could cause delayed I/O operations.
 
-Your root repository for GitHub pages is one named `username.github.io`, for example [arkecosystem.github.io](https://github.com/ArkEcosystem/arkecosystem.github.io) \(repo\). After you've built the explorer by running `yarn build`, you can push the contents of the `dist` folder to your root repository and it will be served on `username.github.io`. That's it!
+## Installation
 
-#### 1.2. Github With variable repository
+> Before continuing with this guide: You will need access to the database of a core instance. The credentials can be specified in the `.env` file under `EXPLORER_DB_*`.
 
-When you don't use a root repository for GitHub pages \(e.g. because it's already in use\), you can still have it hosted in a sub-directory of your GitHub pages website. If you for example create a repository called `my-explorer`, it will become available on `username.github.io/my-explorer`. If you would then build the explorer by running `yarn build --base https://username.github.io/my-explorer/` and push the contents of the generated `dist` files to your repository `my-explorer`, it will be served on your GitHub pages website. The additional `--base` parameter is needed to point the explorer to the correct url, as it will use the root domain by default.
+### Clone
 
-### 2. Run Explorer On Netlify
-
-The explorer is also compatible with [Netlify](https://www.netlify.com). In order to run it on there, you'll need to:
-
-1. link your explorer repository
-2. choose the `master` branch
-3. set `yarn build:mainnet` or \(`devnet`, depending on what you need\) as build command
-4. set `dist` as publish directory
-5. deploy, and your explorer will become visible on your netfily url
-
-### 3. Run Explorer As An Express Server
-
-You can run the explorer as an express server. This makes it a little more light-weight but not needing to have services such as apache or nginx.
+Before getting started you'll have to clone the repository and install the composer and yarn dependencies. The yarn dependencies are needed for the `@arkecosystem/crypto` package which is used to derive multi-signature addresses.
 
 ```bash
-EXPLORER_HOST="127.0.0.1" EXPLORER_PORT="4200" node express-server.js
+git clone https://github.com/ArkEcosystem/explorer.ark.io.git explorer
+cd explorer
+composer install
+yarn install
 ```
 
-### 4. Run Explorer As An ARK Core Plugin
+### Preparing Application
 
-If you run an ARK relay, you can also run an explorer as a core plugin. For this you'll need to follow the following steps:
+Next up is preparing the application by copying all necessary files, generating an application key and migrating the database.
 
-1. Install the core-explorer package: `yarn global add @arkecosystem/core-explorer`
-2. To enable the plugin, add `'@arkecosystem/core-explorer': {}` to the end of your `~/.config/ark-core/<mainnet|devnet|testnet>/plugins.js` file. After this you will need to point the plugin to a directory where it can find the built explorer files. For this you need to edit your `~/.config/ark-core/<mainnet|devnet|testnet>/.env` file and add a `CORE_EXPLORER_PATH` variable followed by the path to your explorer files. This means that you need to build the explorer first, and send the `dist` folder over to your server and use its location for the `CORE_EXPLORER_PATH` variable \(for example `CORE_EXPLORER_PATH=/home/ark/explorer/dist/`\).
-3. Restart the relay and your explorer will become available on your node's ip on port 4200. This will be indicated by a message similar to `Explorer is listening on http://0.0.0.0:4200.` in your logs. For further customizations, the `.env` variables `CORE_EXPLORER_HOST` and `CORE_EXPLORER_PORT` can be set \(e.g. to change the port\).
+```bash
+cp .env.prod .env
+php artisan key:generate
+touch database/database.sqlite
+php artisan migrate:fresh
+php artisan storage:link
+```
 
-### 5. General Website Hosting Notes
+### Configuring Environment
 
-You can also run an explorer on any other server \(e.g. at a website hosting company\), as long as a service like `nginx` or `apache` is installed. The instructions on how to get it up and running will differ per service, but in general it comes down to having the contents of the `dist` folder being served on your website. This could be as easy as having it put inside a `www` or `html` folder, to having to specify configurations with `nginx` or another service. Keep in mind that if you run the explorer from a sub-directory, that you will need to build with the `--base` parameter. You can find more information on this in the README document in the [explorer repository](https://github.com/ArkEcosystem/explorer).
+Finally you'll need to open the `.env` file and edit the following variables. *You should leave all other variables at their defaults.*
+
+```ini
+APP_NAME="Your Explorer Title"
+APP_URL=http://your-domain.com
+
+EXPLORER_NETWORK=production
+EXPLORER_DB_HOST=YOUR_CORE_DATABASE_IP_ADDRESS
+EXPLORER_DB_PORT=YOUR_CORE_DATABASE_PORT
+EXPLORER_DB_DATABASE=YOUR_CORE_DATABASE
+EXPLORER_DB_USERNAME=YOUR_CORE_DATABASE_USERNAME
+EXPLORER_DB_PASSWORD=YOUR_CORE_DATABASE_PASSWORD
+
+; You can find out the path by running "which node" and then use the output.
+EXPLORER_NODEJS=/path/to/your/nodejs/installation
+```
+
+> We do recommend to set `PDO_ATTR_PERSISTENT` to `true` (which is the default) to make use of persistent PostgreSQL connections. This greatly increases the execution time of database queries on pages that execute a lot of queries because the server won't have to establish a new connection for every query. Keep in mind that this requires more resources to keep up the connections but ultimately yields a smoother experienced.
+
+### Cronjobs
+
+Explorer performs a lot of tasks in the background. These tasks are executed on a specific schedule and require the task scheduler to be set up. Take a look at the official [Starting The Scheduler](https://laravel.com/docs/8.x/scheduling#starting-the-scheduler) guide by [Laravel](https://laravel.com/).
+
+> If you are using Laravel Forge you can create this through their "Scheduler" UI.
+
+### Daemons
+
+Explorer performs a lot of tasks in the background. These tasks are executed on a specific schedule and require the task scheduler to be set up. Take a look at the official [Starting The Scheduler](https://laravel.com/docs/8.x/scheduling#starting-the-scheduler) guide by [Spatie](https://laravel.com/).
+
+#### Starting Horizon
+
+Take a look at the official [Deploying Horizon](https://laravel.com/docs/8.x/horizon#deploying-horizon) guide by [Laravel](https://laravel.com/).
+
+> If you are using Laravel Forge you can create this through their "Daemons" UI.
+
+#### Starting Short Schedule
+
+Take a look at the official [Deploying Short Schedule](https://github.com/spatie/laravel-short-schedule#installation) guide by [Laravel](https://spatie.be/).
+
+> If you are using Laravel Forge you can create this through their "Daemons" UI.
+
+### Caching
+
+Now that the task scheduler and Horizon are running you'll need to run the below commands in order to cache all of the data that is required for the Explorer to function.
+
+```bash
+php artisan explorer:cache-network-status
+php artisan explorer:cache-network-aggregates
+php artisan explorer:cache-last-blocks
+php artisan explorer:cache-fees
+php artisan explorer:cache-prices
+php artisan explorer:cache-delegate-aggregates
+php artisan explorer:cache-delegate-performance
+php artisan explorer:cache-delegate-productivity
+php artisan explorer:cache-delegate-resignation-ids
+php artisan explorer:cache-delegate-usernames
+php artisan explorer:cache-delegate-wallets
+php artisan explorer:cache-delegates-with-voters
+php artisan explorer:cache-multi-signature-addresses
+```
+
+## Updates
+
+When updating the Explorer there are a few things to keep in mind. All of them should be executed in the specified order to avoid unexpected issues. You shouldn't
+
+### Restart Horizon
+
+```bash
+php artisan horizon:purge
+php artisan horizon:terminate
+```
+
+### Clear Cache
+
+```bash
+php artisan responsecache:clear
+php artisan view:clear
+```
+
+### Cache Configuration
+
+```bash
+php artisan config:cache
+```
+
+### Cache Routes
+
+```bash
+php artisan route:cache
+```
