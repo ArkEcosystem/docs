debf4be28b1b203f00c33daa7d6e1dbf0598f559
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-11 15:06:23 +0100

    docs(manager-api): configuration.updateEnv method

--- docs/api/manager-api/endpoints/configuration.md.blade.php
+++ docs/api/manager-api/endpoints/configuration.md.blade.php
@@ -50,6 +50,54 @@ configuration.getEnv
 }
 ```
 
+## Update core .env configuration
+
+### Method
+
+```bash
+configuration.updateEnv
+```
+
+### Parameters
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
+| method | string | The method name. | Yes |
+| params | object | The parameters of the request. | Yes |
+| params.content | string | .env file configuration content. | Yes |
+
+### Result
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
+| result | object | Result. | Yes |
+
+### Request
+
+```javascript
+{
+    "id": "unique-request-id",
+	"jsonrpc": "2.0",
+	"method": "configuration.updateEnv",
+	"params": { "content": "CORE_LOG_LEVEL=info\nCORE_LOG_LEVEL_FILE=info\n\nCORE_DB_HOST=localhost\nCORE_DB_PORT=5432\n\nCORE_P2P_HOST=0.0.0.0\nCORE_P2P_PORT=4000\n\nCORE_WEBHOOKS_HOST=0.0.0.0\nCORE_WEBHOOKS_PORT=4004\n\nCORE_MONITOR_HOST=0.0.0.0\nCORE_MONITOR_PORT=4005\n\nCORE_API_HOST=0.0.0.0\nCORE_API_PORT=4003\n"
+    }
+}
+```
+
+### Response
+
+```javascript
+{
+    "id": "unique-request-id",
+    "jsonrpc": "2.0",
+    "result": {}
+}
+```
+
 ## Get core plugins configuration
 
 ### Method
