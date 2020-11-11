ef6313b91955b357f4adae615e395b088430671c
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-11 12:04:15 +0100

    docs(manager-api): info.coreStatus method

--- docs/api/manager-api/endpoints/info.md.blade.php
+++ docs/api/manager-api/endpoints/info.md.blade.php
@@ -16,8 +16,8 @@ info.coreVersion
 
 | Name | Type | Description | Required |
 | :--- | :---: | :--- | :---: |
-| jsonrpc | string | The protocol version. | Yes |
 | id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
 | method | string | The method name. | Yes |
 | params | object | The parameters of the request. | Yes |
 
@@ -50,6 +50,68 @@ info.coreVersion
     "result": {
         "currentVersion": "3.0.0-next.8",
         "latestVersion": "2.7.7"
+    }
+}
+```
+
+## Get core status
+
+### Method
+
+```bash
+info.coreStatus
+```
+
+### Parameters
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
+| method | string | The method name. | Yes |
+| params | object | The parameters of the request. | Yes |
+| params.token | string | Token name. | No |
+
+### Result
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
+| result.status | string | Core / rely process status. | Yes |
+| result.syncing | boolean | Determine if node is currently syncing | No |
+
+### Process statuses
+
+- undefined
+- online
+- stopped
+- stopping
+- waiting restart
+- launching
+- errored
+- one-launch-status
+
+### Request
+
+```javascript
+{
+    "id": "unique-request-id",
+	"jsonrpc": "2.0",
+	"method": "info.coreStatus",
+	"params": { "token": "ark" }
+}
+```
+
+### Response
+
+```javascript
+{
+    "id": "unique-request-id",
+    "jsonrpc": "2.0",
+    "result": {
+        "processStatus": "online",
+        "syncing": false
     }
 }
 ```
