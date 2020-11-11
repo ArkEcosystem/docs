5a001184a6a298ea8aa3be1db4bbb755db028b5f
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-11 13:25:50 +0100

    docs(manager-api): info.currentDelegate method

--- docs/api/manager-api/endpoints/info.md.blade.php
+++ docs/api/manager-api/endpoints/info.md.blade.php
@@ -413,3 +413,54 @@ info.lastForgedBlock
     }
 }
 ```
+
+## Get current delegate
+
+### Method
+
+```bash
+info.currentDelegate
+```
+
+### Parameters
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
+| method | string | The method name. | Yes |
+| params | object | The parameters of the request. | Yes |
+
+### Result
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
+| result | object | Result. | Yes |
+| result.rank | number | Delegate rank. | Yes |
+| result.username | string | Delegate username. | Yes |
+
+### Request
+
+```javascript
+{
+    "id": "unique-request-id",
+	"jsonrpc": "2.0",
+	"method": "info.currentDelegate",
+	"params": {}
+}
+```
+
+### Response
+
+```javascript
+{
+    "id": "unique-request-id",
+    "jsonrpc": "2.0",
+    "result": {
+        "rank": 9,
+        "username": "genesis_30"
+    }
+}
+```
