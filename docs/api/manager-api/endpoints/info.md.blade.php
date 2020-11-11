6740e7c2efb0edc2b36d4a80f45467a53a5fc498
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-11 11:47:29 +0100

    docs(manager-api): info.coreVersion method

--- docs/api/manager-api/endpoints/info.md.blade.php
+++ docs/api/manager-api/endpoints/info.md.blade.php
@@ -3,3 +3,53 @@ title: Info
 ---
 
 # Info
+
+## Get core version
+
+### Method
+
+```bash
+info.coreVersion
+```
+
+### Parameters
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| jsonrpc | string | The protocol version. | Yes |
+| id | string / number | The identifier of the request. | Yes |
+| method | string | The method name. | Yes |
+| params | object | The parameters of the request. | Yes |
+
+### Result
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
+| result.currentVersion | string | Used core version. | Yes |
+| result.latestVersion | string | Latest core version from NPM. | Yes |
+
+### Request
+
+```javascript
+{
+    "id": "unique-request-id",
+	"jsonrpc": "2.0",
+	"method": "info.coreVersion",
+	"params": {}
+}
+```
+
+### Response
+
+```javascript
+{
+    "id": "unique-request-id",
+    "jsonrpc": "2.0",
+    "result": {
+        "currentVersion": "3.0.0-next.8",
+        "latestVersion": "2.7.7"
+    }
+}
+```
