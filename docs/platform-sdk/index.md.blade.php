90839ca71766b7b9fd2230870fb56f806b637396
sleepdefic1t <simonmdowney@gmail.com>
2021-02-12 19:30:37 -0500

    chore(psdk): add dot spec

--- docs/platform-sdk/index.md.blade.php
+++ docs/platform-sdk/index.md.blade.php
@@ -23,6 +23,7 @@
 <x-general.sidebar-link path="/docs/platform-sdk/coins/atom" name="ATOM" />
 <x-general.sidebar-link path="/docs/platform-sdk/coins/avax" name="AVAX" />
 <x-general.sidebar-link path="/docs/platform-sdk/coins/btc" name="BTC" />
+<x-general.sidebar-link path="/docs/platform-sdk/coins/dot" name="DOT" />
 <x-general.sidebar-link path="/docs/platform-sdk/coins/eos" name="EOS" />
 <x-general.sidebar-link path="/docs/platform-sdk/coins/eth" name="ETH" />
 <x-general.sidebar-link path="/docs/platform-sdk/coins/lsk" name="LSK" />
