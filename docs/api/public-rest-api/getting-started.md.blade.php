e3e22c78f1787526128a0cb5270dde28b4fe6513
rigwild <me@rigwild.dev>
2021-02-19 07:57:37 +0100

    Update getting-started.md.blade.php

--- docs/api/public-rest-api/getting-started.md.blade.php
+++ docs/api/public-rest-api/getting-started.md.blade.php
@@ -10,8 +10,8 @@ All HTTP requests have to be sent with the `Content-Type: application/json` head
 
 This is the reference guide for the Public API. This API exposes all resources and data provided by an ARK Core node; and is the preferred way of interacting with the ARK network. Note that each node has its own internal blockchain and state, meaning it may have forked or be out of sync, causing queries to fail. Monitor your node by comparing it to different public nodes, such as an official explorer to ensure you are in sync.
 
- * Mainnet: [https://explorer.ark.io](https://explorer.ark.io)
- * Devnet: [https://dexplorer.ark.io](https://dexplorer.ark.io)
+* Mainnet: [https://explorer.ark.io](https://explorer.ark.io)
+* Devnet: [https://dexplorer.ark.io](https://dexplorer.ark.io)
 
 > If you have any problems or requests please [open an issue](https://github.com/ARKEcosystem/core/issues/new/choose).
 
@@ -23,7 +23,7 @@ Requests that return multiple items will be paginated to 100 items by default. Y
 
 If you are not running a relay yourself you can test API calls using:
 
- * Mainnet: [https://api.ark.io/api](https://api.ark.io/api)
- * Devnet: [https://dwallets.ark.io/api](https://dwallets.ark.io/api)
+* Mainnet: [https://api.ark.io/api](https://api.ark.io/api)
+* Devnet: [https://dwallets.ark.io/api](https://dwallets.ark.io/api)
 
 Happy developing!
