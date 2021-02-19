45e6fb64d5c098023196b95759a7ead17bc3da55
rigwild <me@rigwild.dev>
2021-02-19 02:02:36 +0100

    Update getting-started.md.blade.php

--- docs/api/public-rest-api/getting-started.md.blade.php
+++ docs/api/public-rest-api/getting-started.md.blade.php
@@ -8,7 +8,10 @@ title: Getting Started
 All HTTP requests have to be sent with the `Content-Type: application/json` header. If the header is not present it will result in malformed responses or request rejections.
 </x-alert>
 
-This is the reference guide for the Public API. This API exposes all resources and data provided by an ARK Core node; and is the preferred way of interacting with the ARK network. Note that each node has its own internal blockchain and state, meaning it may have forked or be out of sync, causing queries to fail. Monitor your node by comparing it to different public nodes, such as the official [explorer](https://explorer.ark.io:8443/api) to ensure you are in sync.
+This is the reference guide for the Public API. This API exposes all resources and data provided by an ARK Core node; and is the preferred way of interacting with the ARK network. Note that each node has its own internal blockchain and state, meaning it may have forked or be out of sync, causing queries to fail. Monitor your node by comparing it to different public nodes, such as an official to ensure you are in sync.
+
+ * Mainnet: [https://explorer.ark.io](https://explorer.ark.io)
+ * Devnet: [https://dexplorer.ark.io](https://dexplorer.ark.io)
 
 > If you have any problems or requests please [open an issue](https://github.com/ARKEcosystem/core/issues/new/choose).
 
@@ -20,7 +23,7 @@ Requests that return multiple items will be paginated to 100 items by default. Y
 
 If you are not running a relay yourself you can test API calls using:
 
- - For mainnet: [https://api.ark.io/api](https://api.ark.io/api) (explorer: [https://explorer.ark.io/](https://explorer.ark.io/))
- - For devnet: [https://dwallets.ark.io/api](https://dwallets.ark.io/api) (explorer: [https://dexplorer.ark.io/](https://dexplorer.ark.io/))
+ * Mainnet: [https://api.ark.io/api](https://api.ark.io/api)
+ * Devnet: [https://dwallets.ark.io/api](https://dwallets.ark.io/api)
 
 Happy developing!
