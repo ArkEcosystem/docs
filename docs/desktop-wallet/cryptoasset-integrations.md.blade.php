55100ed75589b5897ba65241675fe42700d67f26
sleepdefic1t <simonmdowney@gmail.com>
2021-03-05 01:04:12 -0500

    docs(dw): update cryptoasset page
    - use relative doc links
    - resolve markdown lint warning ln#20
    - remove trailing white space
    - add AVAX to coins list
    - add DOT to coins list
    - add EGLD to coins list
    - add SOL to coins list

--- docs/desktop-wallet/cryptoasset-integrations.md.blade.php
+++ docs/desktop-wallet/cryptoasset-integrations.md.blade.php
@@ -4,34 +4,38 @@ title: Cryptoasset Integrations
 
 # Cryptoasset Integrations
 
-The [ARK Desktop Wallet](https://ark.io/wallet) uses the [ARK Platform-SDK](https://ark.dev/docs/platform-sdk) to allow the integration of multiple assets within the wallet. This document will outline the current list of supported assets as well as planned additions to our wallet. 
+The [ARK Desktop Wallet](https://ark.io/wallet) uses the [ARK Platform-SDK](/docs/platform-sdk) to allow the integration of multiple assets within the wallet. This document will outline the current list of supported assets as well as planned additions to our wallet.
 
 ## Supported Cryptoassets
 
-The ARK Desktop Wallet currently supports the following cryptoassets: 
+The ARK Desktop Wallet currently supports the following cryptoassets:
 
-- [ARK](https://ark.dev/docs/platform-sdk/coins/ark)
-- [LSK](https://ark.dev/docs/platform-sdk/coins/lsk)
+- [ARK](/docs/platform-sdk/coins/ark)
+- [LSK](/docs/platform-sdk/coins/lsk)
 
 ## Planned Integrations
 
-The following cryptoassets are already in the initial stages of integration within the [ARK Platform-SDK](https://ark.dev/docs/platform-sdk) and will be added to the [ARK Desktop Wallet](https://ark.io/wallet) upon completion. 
+The following cryptoassets are already in the initial stages of integration within the [ARK Platform-SDK](/docs/platform-sdk) and will be added to the [ARK Desktop Wallet](https://ark.io/wallet) upon completion.
 
-*(Assets are listed in alphabetical order)*
+_*(Assets are listed in alphabetical order)*_
 
-- [ADA](https://ark.dev/docs/platform-sdk/coins/ada)
-- [ATOM](https://ark.dev/docs/platform-sdk/coins/atom)
-- [BTC](https://ark.dev/docs/platform-sdk/coins/btc)
-- [EOS](https://ark.dev/docs/platform-sdk/coins/eos)
-- [ETH](https://ark.dev/docs/platform-sdk/coins/eth)
-- [NEO](https://ark.dev/docs/platform-sdk/coins/neo)
-- [TRX](https://ark.dev/docs/platform-sdk/coins/trx)
-- [XLM](https://ark.dev/docs/platform-sdk/coins/xlm)
-- [XRP](https://ark.dev/docs/platform-sdk/coins/xrp)
+- [ADA](/docs/platform-sdk/coins/ada)
+- [ATOM](/docs/platform-sdk/coins/atom)
+- [AVAX](/docs/platform-sdk/coins/avax)
+- [BTC](/docs/platform-sdk/coins/btc)
+- [DOT](/docs/platform-sdk/coins/dot)
+- [EGLD](/docs/platform-sdk/coins/egld)
+- [EOS](/docs/platform-sdk/coins/eos)
+- [ETH](/docs/platform-sdk/coins/eth)
+- [NEO](/docs/platform-sdk/coins/neo)
+- [SOL](/docs/platform-sdk/coins/sol)
+- [TRX](/docs/platform-sdk/coins/trx)
+- [XLM](/docs/platform-sdk/coins/xlm)
+- [XRP](/docs/platform-sdk/coins/xrp)
 
 ## Integration Requests
 
-We are currently not taking community integration requests. If you would like a specific cryptoasset to be added to the [ARK Desktop Wallet](https://ark.io/wallet), please encourage them to register at [MarketSquare](https://marketsquare.io) and reach out to us directly. 
+We are currently not taking community integration requests. If you would like a specific cryptoasset to be added to the [ARK Desktop Wallet](https://ark.io/wallet), please encourage them to register at [MarketSquare](https://marketsquare.io) and reach out to us directly.
 
 We will add new assets based on a series of factors to include:
 
@@ -41,10 +45,9 @@ We will add new assets based on a series of factors to include:
 - MarketSquare Participation
 
 <x-alert type="warning">
-The ARK.io Team does not charge a listing fee and will not ask for Coins, Tokens, or other payment for listing. If someone is asking you for a deposit, you are being scammed. 
+The ARK.io Team does not charge a listing fee and will not ask for Coins, Tokens, or other payment for listing. If someone is asking you for a deposit, you are being scammed.
 </x-alert>
 
 ## Contact Us
 
-For more information or to inquire about contributing to the [ARK Platform-SDK](https://ark.dev/docs/platform-sdk), please feel free to [Contact Us](https://ark.dev/contact). 
-
+For more information or to inquire about contributing to the [ARK Platform-SDK](/docs/platform-sdk), please feel free to [Contact Us](https://ark.dev/contact).
