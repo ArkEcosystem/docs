13f1a90c7a7ec3aec1b99411c8e022cd1f8077f7
Brian Faust <hello@basecode.sh>
2021-04-12 06:15:27 +0300

    wip

---
+++ docs/platform-sdk/crypto/uuid.md.blade.php
@@ -0,0 +1,55 @@
+---
+title: UUID
+---
+
+# UUID
+
+The `UUID` class provides methods to make working with UUIDs easier and consistent across all Platform SDK products.
+
+### Importing the UUID class
+
+```typescript
+import { UUID } from "@arkecosystem/platform-sdk-crypto";
+```
+
+### Generate a UUID based on the current time
+
+```typescript
+UUID.timestamp();
+```
+
+### Generate a MD5 UUID based on a name and namespace
+
+```typescript
+UUID.md5('name', 'namespace');
+```
+
+### Generate a UUID based on a random value
+
+```typescript
+UUID.random();
+```
+
+### Generate a SHA1 UUID based on a name and namespace
+
+```typescript
+UUID.sha1('name', 'namespace');
+```
+
+### Generate a UUID based on a parse value
+
+```typescript
+UUID.parse('9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d');
+```
+
+### Convert a buffer into a UUID
+
+```typescript
+UUID.stringify(UUID.parse('9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'));
+```
+
+### Validate a UUID
+
+```typescript
+UUID.validate('9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d');
+```
---
+++ docs/platform-sdk/crypto/wif.md.blade.php
@@ -0,0 +1,29 @@
+---
+title: WIF
+---
+
+# WIF
+
+The `WIF` class provides methods to make working with the `Bitcoin Wallet Import Format` easier and consistent across all Platform SDK products.
+
+### Importing the WIF class
+
+```typescript
+import { WIF } from "@arkecosystem/platform-sdk-crypto";
+```
+
+### Encode a private key
+
+```typescript
+WIF.encode({
+    version: 128,
+    privateKey: "0000000000000000000000000000000000000000000000000000000000000001",
+    compressed: true,
+});
+```
+
+### Decode an encoded WIF
+
+```typescript
+WIF.decode('KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73sVHnoWn');
+```
