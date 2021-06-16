627265b25b761b95b511bdb64e403f5566f1e2d8
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-06-16 16:21:23 -0400

    chore(incentives): update intro

--- docs/program-incentives/intro.md.blade.php
+++ docs/program-incentives/intro.md.blade.php
@@ -1,14 +1,29 @@
+---
+title: Introduction
+---
+
 # Introduction
 
 ## ARK Grants
+
 ARK Grants is a developer incentive program backed by a dedicated 1 million ARK fund. This program enables developers to get funding for building ARK-powered Proofs-of-Concept, applications, and plugins. Applying is a simple process, and projects are made open source to benefit the developer community.
+
 <livewire:page-reference path="/docs/program-incentives/ark-grants-program-guidelines" />
 
-## Development and Security Bounty Program
-Developers, auditors, and bug hunters may earn ARK bounties for discovering and reporting security vulnerabilities and bugs in the ARK codebase. Bounties are handled on a case-by-case basis based on the nature and severity of the discovery. Members of the program do their part to make the ARK codebase secure and robust, and joining the program is simple.
-<livewire:page-reference path="/docs/program-incentives/bounty" />
+## Development Bounty Program
+
+Developers may earn ARK bounties for discovering bugs in the ARK codebase. Bounties are handled on a case-by-case basis based on the nature and complexity of the discovery. Contributors to the 'Development Bounty Program' do their part to make the ARK codebase secure and robust. Click below to learn more.
+
+<livewire:page-reference path="/docs/program-incentives/development-bounty-program" />
+
+## Security Vulnerability Program
+
+Developers, auditors, and bug hunters may earn ARK bounties for discovering and reporting security vulnerabilities in the ARK codebase. Reported vulnerabilities are handled on a case-by-case basis based on the nature and severity of the discovery. Contributors to the 'Security Vulnerability' program do their part to make the ARK codebase secure and robust, and joining the program is simple.
+
+<livewire:page-reference path="/docs/program-incentives/security-vulnerability-program" />
 
 ## Contribution Guidelines
+
 Developers are expected to follow contribution guidelines to keep the codebase efficient, readable, and standardized. Contribution guidelines are clearly laid out so developers and contributors can submit their work without much revision, resulting in faster development and more useful contributions. Check out these guidelines before contributing code to the project.
 <x-link-collection
     :links="[
