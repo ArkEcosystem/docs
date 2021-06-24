921c0868ead37466790354b86e72a29550f08a1b
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-06-24 15:22:48 -0400

    chore(core): tx overview - fix sdk links

--- docs/core/transactions/transaction-types/overview.md.blade.php
+++ docs/core/transactions/transaction-types/overview.md.blade.php
@@ -12,13 +12,13 @@ This sections describes Mainnet Transaction Types and its structure related to t
 Transactions are the heart of any blockchain, cryptocurrency or otherwise. They represent a transfer of value from one network participant to another. In ARK, transactions can be of one of multiple types, specified in AIP11, which can affect the content and data structure of each transaction's payload.
 </x-alert>
 
-Using the [ARK SDKs](/docs/api/), developers can employ the programming language of their choice to build applications utilizing the ARK blockchain. The ARK SDKs are split into two packages for each language: Client and Cryptography.
+Using the [ARK SDKs](/docs/sdk/), developers can employ the programming language of their choice to build applications utilizing the ARK blockchain. The ARK SDKs are split into two packages for each language: Client and Cryptography.
 
 **Client** SDKs help developers fetch information from the ARK blockchain about its current state: which delegates are currently forging, what transactions are associated with a given wallet, and so on.
 
 **Cryptography** SDKs, by contrast, assist developers in working with transactions: signing, serializing, deserializing, etc.
 
-For more information about SDK implementations visit [ARK SDKs hub](/docs/api/).
+For more information about SDK implementations visit [ARK SDKs hub](/docs/sdk/).
 
 In the following sections basic transaction types and their structure is presented. If you are interested in the signature generation process and algorithm used, please check the [Cryptography Overview](/docs/core/transactions/cryptography-overview) page.
 
