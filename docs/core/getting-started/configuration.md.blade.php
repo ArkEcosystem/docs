a57d9e6bb2b14eee0ded209e1d19a4de422ae4e4
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-06-28 08:24:46 +0200

    docs(core): update configuration page

--- docs/core/getting-started/configuration.md.blade.php
+++ docs/core/getting-started/configuration.md.blade.php
@@ -4,60 +4,124 @@ title: Configuration
 
 # Configuration
 
-ARK Core provides a wide variety of ways to configure how it behaves, what plugins are used, who is forging and more. Each option is documented, so head over to the [ARK Core repository](https://github.com/ArkEcosystem/core/) and dive in. All of this configuration can be found in the `app.js`, `delegates.json` and `peers.json` files of the respective network you are using.
+ARK Core provides a wide variety of ways to configure how it behaves, what plugins are used, who is forging and more. Each option is documented, so head over to the [ARK Core repository](https://github.com/ArkEcosystem/core/) and dive in. All of this configuration can be found in the `app.json`, `delegates.json` and `peers.json` files of the respective network you are using.
 
-## Service Configuration
-
-Services are the building blocks that make up all of the internals of Core that make it possible for it to do what it does. Think of things like logging, caching, events and task scheduling; all of those are necessary for Core to function but they aren't plugins so how do we configure them?
-
-```javascript
-module.exports = {
-    services: {
-        log: {
-            levels: {
-                console: process.env.CORE_LOG_LEVEL || "emergency",
-                file: process.env.CORE_LOG_LEVEL_FILE || "emergency",
-            },
-            fileRotator: {
-                interval: "1d",
-            },
-        },
-    }
-};
-```
-
-As you can see the configuration format is quite simple. We have a `services` key that serves as a namespace for all of our service specific configuration. Then we can see the `log` key, internally the log service will look for this key and use its value to configure things like log rotations and what severity messages go to which output stream.
-
-## Plugin Configuration
+## Plugins Configuration
 
 Plugins are packages that expose services which expand or alter what Core is capable of doing, by default there are a few required plugins like the a database driver, transaction pool, peer communication and consensus.
 
-```javascript
-module.exports = {
-    plugins: [{
-        package: "@arkecosystem/core-state"
+```json
+{
+    "core": {
+        "plugins": [
+            {
+                "package": "@arkecosystem/core-logger-pino"
+            },
+            {
+                "package": "@arkecosystem/core-state"
+            },
+            {
+                "package": "@arkecosystem/core-database"
+            },
+            {
+                "package": "@arkecosystem/core-transactions"
+            },
+            {
+                "package": "@arkecosystem/core-magistrate-transactions"
+            },
+            {
+                "package": "@arkecosystem/core-transaction-pool"
+            },
+            {
+                "package": "@arkecosystem/core-p2p"
+            },
+            {
+                "package": "@arkecosystem/core-blockchain"
+            },
+            {
+                "package": "@arkecosystem/core-api"
+            },
+            {
+                "package": "@arkecosystem/core-magistrate-api"
+            },
+            {
+                "package": "@arkecosystem/core-webhooks"
+            },
+            {
+                "package": "@arkecosystem/core-forger"
+            }
+        ]
     },
-    {
-        package: "@arkecosystem/core-database"
+    "relay": {
+        "plugins": [
+            {
+                "package": "@arkecosystem/core-logger-pino"
+            },
+            {
+                "package": "@arkecosystem/core-state"
+            },
+            {
+                "package": "@arkecosystem/core-database"
+            },
+            {
+                "package": "@arkecosystem/core-transactions"
+            },
+            {
+                "package": "@arkecosystem/core-magistrate-transactions"
+            },
+            {
+                "package": "@arkecosystem/core-transaction-pool"
+            },
+            {
+                "package": "@arkecosystem/core-p2p"
+            },
+            {
+                "package": "@arkecosystem/core-blockchain"
+            },
+            {
+                "package": "@arkecosystem/core-api"
+            },
+            {
+                "package": "@arkecosystem/core-magistrate-api"
+            },
+            {
+                "package": "@arkecosystem/core-webhooks"
+            }
+        ]
     },
-    {
-        package: "@arkecosystem/core-transaction-pool"
-    },
-    {
-        package: "@arkecosystem/core-p2p",
-        options: {
-            minimumNetworkReach: 5,
-        }
-    },
-    {
-        package: "@arkecosystem/core-blockchain"
-    }]
-};
+    "forger": {
+        "plugins": [
+            {
+                "package": "@arkecosystem/core-logger-pino"
+            },
+            {
+                "package": "@arkecosystem/core-forger"
+            }
+        ]
+    }
+}
 ```
 
-Again the configuration is quite simple. We have a `plugins` key that serves as a namespace for all of our plugin specific configuration. Then we see a list of objects that all contain `package` key and an optional `options` key.
+Again the configuration is quite simple. We have a `plugins` key that serves as a namespace for all of our plugin specific configuration, for each process type. Then we see a list of objects that all contain `package` key and an optional `options` key.
 
-The `package` key will be used by Core to decide what package should be loaded, this can be either an npm package name or path to a local package. The `options` key will be used to configure the services that are provided by the plugin.
+The `package` key is used by Core to decide what package should be loaded, this can be either an npm package name or path to a local package.
+
+### Plugin options
+
+The `options` key is used to configure the services that are provided by the plugin. Provided options are merged with default package configuration that is loaded from "default.ts" file inside the package.
+
+Keys that are type of object will be merged with default configuration. String, number, boolean and array keys, will replace default configuration key.
+
+```json
+{
+    "package": "@arkecosystem/core-logger-pino",
+    "options": {
+        "fileRotator": {
+            "interval": "2d"
+        }
+    }
+},
+```
 
 ## Delegate Configuration
 
