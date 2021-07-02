f84a3716110791f1e4af36eddf39708bc917a1e9
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-07-02 11:13:47 +0200

    chore(api): remove search wallet endpoints

--- docs/api/public-rest-api/endpoints/wallets.md.blade.php
+++ docs/api/public-rest-api/endpoints/wallets.md.blade.php
@@ -20,6 +20,4 @@ A Wallet resource is reachable using an associated Address or PublicKey.
 
 <livewire:endpoint spec="api/public-rest-api/endpoints/specs/wallets/transactions/received.json" />
 
-<livewire:endpoint spec="api/public-rest-api/endpoints/specs/wallets/search.json" />
-
 <livewire:endpoint spec="api/public-rest-api/endpoints/specs/wallets/votes.json" />
