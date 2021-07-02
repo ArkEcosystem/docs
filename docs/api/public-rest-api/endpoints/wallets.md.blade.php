8274de1b1d44279f374fbd034a09f306f873e7fc
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-07-02 17:18:16 +0200

    chore(api): update locks endpoints

--- docs/api/public-rest-api/endpoints/wallets.md.blade.php
+++ docs/api/public-rest-api/endpoints/wallets.md.blade.php
@@ -21,3 +21,5 @@ A Wallet resource is reachable using an associated Address or PublicKey.
 <livewire:endpoint spec="api/public-rest-api/endpoints/specs/wallets/transactions/received.json" />
 
 <livewire:endpoint spec="api/public-rest-api/endpoints/specs/wallets/votes.json" />
+
+<livewire:endpoint spec="api/public-rest-api/endpoints/specs/wallets/locks.json" />
