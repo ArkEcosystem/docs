170513d0c0ae556b6dc06a2d7a59903218944a1d
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-07-08 21:03:21 -0400

    chore: create 'password-reset' page

---
+++ docs/marketsquare/accounts/password-reset.md.blade.php
@@ -0,0 +1,36 @@
+---
+title: Password Reset
+---
+
+# Resetting a MarketSquare Account's Password
+
+An account with a lost or forgotten password can be recovered by using your email address to reset the password.
+
+## Getting Started
+
+To get started, click the 'Sign In' button at the top right of the MarketSquare Homepage.
+
+## The MarketSquare Sign In Page
+
+Click the **'[Forgot password?](https://marketsquare.io/forgot-password)'** link below the **'Password'** field.
+
+## The Password Reset Page
+
+Enter your email address, then click the **'Send Reset Link'** button.
+
+<x-alert type="info">
+Make sure to provide the email address currently associated with your account.
+</x-alert>
+
+## The Recovery Email
+
+
+## Setting a New Password
+
+**Password Requirements:**
+
+- One lowercase character
+- One uppercase character
+- One number
+- One special character
+- 12 characters minimum
