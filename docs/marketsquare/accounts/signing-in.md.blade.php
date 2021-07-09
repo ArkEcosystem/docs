2ab7e8bcd15dc69f96d38edb2a5a02a164ccb6ea
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-07-08 21:02:53 -0400

    chore: create 'signing-in' page

---
+++ docs/marketsquare/accounts/signing-in.md.blade.php
@@ -0,0 +1,15 @@
+---
+title: Signing In
+---
+
+# Singing into a MarketSquare Account
+
+An account with a lost or forgotten password can be recovered by using your email address to reset the password.
+
+## Getting Started
+
+To get started, click the 'Sign In' button at the top right of the MarketSquare Homepage.
+
+## The MarketSquare Sign In Page
+
+Enter your account's Email address or Username, then click the **'Sign In'** button.
