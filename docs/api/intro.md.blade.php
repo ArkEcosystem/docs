b30625f30e72323617ecf46ebfdd29861a0e5c79
Simon <sleepdefic1t@users.noreply.github.com>
2021-07-12 20:34:16 -0400

    chore(manager-api): add api intro link (#146)

--- docs/api/intro.md.blade.php
+++ docs/api/intro.md.blade.php
@@ -8,6 +8,7 @@ This is a reference guide for the available Core APIs. APIs expose different res
 
 1. [Public REST API](/docs/api/public-rest-api/getting-started)
 2. [Webhook API](/docs/api/webhook-api/getting-started)
+2. [Manager API](/docs/api/manager-api/getting-started)
 
 <x-alert type="info">
 Each Core server (node) has its own internal blockchain and state, meaning it may have forked or be out of sync, causing queries to fail. Monitor your node by comparing it to different public nodes, such as the official [explorer](https://explorer.ark.io:8443/api) to ensure you are in sync.
