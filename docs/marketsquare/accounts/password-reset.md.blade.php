228bf1e1cbc877891093e57346bb2bd1ff37c220
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-07-22 13:29:55 -0400

    docs(msq): add password reset images

--- docs/marketsquare/accounts/password-reset.md.blade.php
+++ docs/marketsquare/accounts/password-reset.md.blade.php
@@ -10,13 +10,21 @@ An account with a lost or forgotten password can be recovered by using your emai
 
 To get started, click the **'Sign In'** button at the top right of the MarketSquare Homepage.
 
+![](/storage/docs/docs/marketsquare/assets/accounts/accounts-sign-in-focus.png)
+
 ## The MarketSquare Sign In Page
 
 Click the **'[Forgot password?](https://marketsquare.io/forgot-password)'** link below the **'Password'** field.
 
+![](/storage/docs/docs/marketsquare/assets/accounts/accounts-password-forgot-focus.png)
+
 ## The Password Reset Page
 
 Enter your email address, then click the **'Send Reset Link'** button.
+
+![](/storage/docs/docs/marketsquare/assets/accounts/accounts-password-reset-page.png)
+
+![](/storage/docs/docs/marketsquare/assets/accounts/accounts-password-reset-confirmation.png)
 
 <x-alert type="info">
 Make sure to provide the email address currently associated with your account.
@@ -24,13 +32,36 @@ Make sure to provide the email address currently associated with your account.
 
 ## The Recovery Email
 
+You'll then receive a confirmation email at the address you provided. If you don't see the email, check your spam folder and also make sure you provided the correct address.
+
+Click the email's **'Reset Password'** button to continue.
+
+![](/storage/docs/docs/marketsquare/assets/accounts/accounts-password-reset-email.png)
 
 ## Setting a New Password
 
-**Password Requirements:**
+You'll now be able to set your new password.
+
+![](/storage/docs/docs/marketsquare/assets/accounts/accounts-password-set-new.png)
+
+**Password Requirements**:
 
 - One lowercase character
 - One uppercase character
 - One number
 - One special character
 - 12 characters minimum
+
+When you're finished entering and confirming your new password, click the **'Reset Password'** button.
+
+## Sign In Using Your New Password
+
+You'll then be redirected to the **'Sign In'** page.
+
+Enter your account's Email or Username and new Password, then click the **'Sign In'** button.
+
+![](/storage/docs/docs/marketsquare/assets/accounts/accounts-password-reset-sign-in.png)
+
+<x-alert type="success">
+Congrats!! You've successfully reset your password and recovered your account!
+</x-alert>
