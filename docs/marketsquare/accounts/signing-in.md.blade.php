da0851cba84454d156f40d0df06779b035b68c63
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-07-22 13:29:34 -0400

    docs(msq): add sign in images

--- docs/marketsquare/accounts/signing-in.md.blade.php
+++ docs/marketsquare/accounts/signing-in.md.blade.php
@@ -8,6 +8,10 @@ title: Signing In
 
 To get started, click the **'Sign In'** button at the top right of the MarketSquare Homepage.
 
+![](/storage/docs/docs/marketsquare/assets/accounts/accounts-sign-in-focus.png)
+
 ## The MarketSquare Sign In Page
 
 Enter your account's Email or Username and Password, then click the **'Sign In'** button.
+
+![](/storage/docs/docs/marketsquare/assets/accounts/accounts-sign-in-page.png)
