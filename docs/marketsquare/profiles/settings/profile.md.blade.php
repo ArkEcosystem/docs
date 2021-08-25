26e3ffe271a5b090544369d7f92bee5080bc8aec
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-08-25 17:42:34 -0400

    docs(msq): add profile settings page

---
+++ docs/marketsquare/profiles/settings/profile.md.blade.php
@@ -0,0 +1,72 @@
+---
+title: Settings - Profile
+---
+
+# Profile Settings
+
+The Profile settings page is where you can update your username or time-zone settings, export your personal data, or delete your account.
+
+![](/storage/docs/docs/marketsquare/assets/profiles/profiles-settings-user-page.png)
+
+## Getting Started
+
+To navigate to your profile's settings page, select ‘**Settings**’ from the Profile drop-down menu.
+
+![](/storage/docs/docs/marketsquare/assets/profiles/profiles-settings-focus.png)
+
+## User Information
+
+This section is where you can change your username or email address.
+
+- **Username**: used for signing into MarketSquare.
+- **Email Address**: used to receive notifications, updates, and announcements.
+
+<x-alert type="warning">
+A username can only be changed once every 30 days.
+</x-alert>
+
+Save any changes by clicking the '**Update**' button.
+
+![](/storage/docs/docs/marketsquare/assets/profiles/profiles-settings-user-information.png)
+
+---
+
+## Timezone
+
+Select your Timezone from the drop-down menu.
+
+Make sure to save your changes by clicking the '**Update**' button.
+
+![](/storage/docs/docs/marketsquare/assets/profiles/profiles-settings-user-timezone.png)
+
+---
+
+## GDPR
+
+This is where you can export all of your personal data that's on MarketSquare.
+
+After clicking **'Export Personal Data'**, a zip file will be sent to the email address associated with your MarketSquare account.
+
+![](/storage/docs/docs/marketsquare/assets/profiles/profiles-settings-user-gdpr.png)
+
+---
+
+## Account Deletion
+
+Once an account is deleted, all of its resources and data will be permanently deleted. This includes **all** Projects _created_ by this account. Back up any important data or information before deleting your account.
+
+<x-alert type="danger">
+Deleting an Account is **PERMANENT**. Make sure all important Profile or Project-related content is saved before proceeding. **This action cannot be reversed**.
+</x-alert>
+
+---
+
+Get started by clicking the **'Delete Account'** button.
+
+![](/storage/docs/docs/marketsquare/assets/profiles/profiles-settings-user-account-delete.png)
+
+---
+
+To confirm the deletion, enter your Username and click the ‘**Delete**’ button.
+
+![](/storage/docs/docs/marketsquare/assets/profiles/profiles-settings-user-account-delete-confirm.png)
