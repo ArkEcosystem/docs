2b7e08557e557ea5570fcadced5cd5565ffb2228
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-09-06 07:43:07 +0200

    Revert "docs(msq): update profile pages (#166)" (#171)
    This reverts commit 238df77447a98b064b1803b5b237dd8ced89d8f5.

--- docs/marketsquare/getting-started/what-next.md.blade.php
+++ docs/marketsquare/getting-started/what-next.md.blade.php
@@ -10,13 +10,13 @@ title: What's Next?
 
 ---
 
-After [creating an Account](/docs/marketsquare/getting-started/account-sign-up) on MarketSquare, you can [customize your profile](/docs/marketsquare/profiles/update), explore the growing list of Projects & Hubs, keep up with the latest news and announcements, [add your own Projects](/docs/marketsquare/projects/creating-projects), and interact with others in the blockchain community! Make sure to update [your settings](/docs/marketsquare/profiles/settings) to secure your account too!
+After [creating an Account](/docs/marketsquare/getting-started/account-sign-up) on MarketSquare, you can [customize your profile](/docs/marketsquare/profiles/updating-profile), explore the growing list of Projects & Hubs, keep up with the latest news and announcements, [add your own Projects](/docs/marketsquare/projects/creating-projects), and interact with others in the blockchain community! Make sure to update [your settings](/docs/marketsquare/profiles/settings) to secure your account too!
 
 <!-- markdownlint-disable MD026 -->
 ## Customize Your Profile!
 <!-- markdownlint-enable MD026 -->
 
-[Customizing your Profile](/docs/marketsquare/profiles/update) on MarketSquare is easy. Change your username, upload a personal avatar, and share more about yourself with a personal bio or website, social media and repository links.
+[Customizing your Profile](/docs/marketsquare/profiles/updating-profile) on MarketSquare is easy. Change your username, upload a personal avatar, and share more about yourself with a personal bio or website, social media and repository links.
 
 ![](/storage/docs/docs/marketsquare/assets/profiles/profiles-overview.png)
 
