90f75fcd708f9de6908673c7b1b7556947dd6c5f
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-09-21 21:53:13 -0400

    chore(exchange): remove old docs

--- docs/exchanges/json-rpc/endpoints/intro.md.blade.php
+++
@@ -1,7 +0,0 @@
----
-title: JSON-RPC Endpoints
----
-
-<livewire:page-reference path="/docs/exchanges/json-rpc/endpoints/blocks" />
-<livewire:page-reference path="/docs/exchanges/json-rpc/endpoints/transactions" />
-<livewire:page-reference path="/docs/exchanges/json-rpc/endpoints/wallets" />
