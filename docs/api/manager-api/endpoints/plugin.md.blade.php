0d7c3c349989e88c1a030966c14746bbac87b705
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-10-25 17:29:22 +0200

    docs: plugin.remove

--- docs/api/manager-api/endpoints/plugin.md.blade.php
+++ docs/api/manager-api/endpoints/plugin.md.blade.php
@@ -157,3 +157,52 @@ plugin.update
 }
 ```
 
+## Remove plugin
+
+### Method
+
+```bash
+plugin.remove
+```
+
+### Parameters
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
+| method | string | The method name. | Yes |
+| params | object | The parameters of the request. | Yes |
+| params.name | string | The plugin name. | Yes |
+
+### Result
+
+| Name | Type | Description | Required |
+| :--- | :---: | :--- | :---: |
+| id | string / number | The identifier of the request. | Yes |
+| jsonrpc | string | The protocol version. | Yes |
+| result | object | Result. | Yes |
+
+### Request
+
+```javascript
+{
+    "id": "unique-request-id",
+    "jsonrpc": "2.0",
+    "method": "plugin.remove",
+    "params": {
+        "name": "@arkecosystem/core-manager"
+    }
+}
+```
+
+### Response
+
+```javascript
+{
+    "id": "unique-request-id",
+    "jsonrpc": "2.0",
+    "result": {}
+}
+```
+
