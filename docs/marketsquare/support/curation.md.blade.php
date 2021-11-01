9cfb526e3ea955dd9ae8ac436a8d6e4fefac9a70
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2021-11-01 16:35:11 -0400

    docs(msq): add asset curation page

---
+++ docs/marketsquare/support/curation.md.blade.php
@@ -0,0 +1,29 @@
+---
+title: MarketSquare Assets & Curation
+---
+
+# MarketSquare Assets & Manual Curation
+
+MarketSquare wants to ensure an inclusive environment for projects across the entire Blockchain industry. Cryptoassets are an integral part of any blockchain project but bring with them unique challenges when we look at inclusion within MarketSquare. In order to ensure the fairest and trustworthy solution for everyone, all cryptoasset listings will be manually curated and controlled by our team.
+
+This document will explain our reasoning and our internal process for researching and curating new assets.
+
+## Why Manually Curate?
+
+Modern blockchains and their related cryptoassets were created to encourage decentralization. The goal is to have no central authority for the network and no central bank or authority for the asset. As such, it was hard from a 3rd party website perspective to feel confident or comfortable assigning one specific team, developer, or person as the "owner" of a decentralized currency.
+
+With a traditional business, it is clear who is responsible, and we feel more comfortable with their personal marketing angle for presenting their product to users. For a decentralized network, who's narrative do you allow to set the precedent? What if the original developer and the community do not agree? What if the original creator no longer exists and multiple teams are developing clients? Who would own Bitcoin? These questions were the first step in our decision to manually curate our cryptoasset listings.
+
+The second major influence on our decision was the underlying principles with which we started MarketSquare. Our goal is always to provide accurate and truthful information to allow everyone to make informed decisions on which projects they choose to support. Unfortunately, when it comes to decentralized blockchains and native cryptoassets or ERC20 Tokens, it is not uncommon to see misinformation or manipulated information, even from the teams responsible for the technology.
+
+We feel that it is important to ensure a positive experience for our users and be able to stand by the information we present to our community. With these factors in mind, we have decided to manually curate each and every asset that is added to MarketSquare.
+
+## Research and Analysis
+
+For all cryptoassets, our internal team conducts a full and in-depth research project wherein we review and analyze data from all major industry sources. In addition, we reach out to parties involved with the project if necessary, ask questions, speak to the community, and use any and all resources available to ensure the data we provide is factual. We also make a concerted effort to avoid any form of opinionated topic or area in which our analysis may be prone to opinion or our own bias.
+
+That means that when we look at assets, we focus on the specific historical and factual elements that can inform the reader while avoiding discussing the validity or applicability of a utility case or product itself. This allows us to provide an accurate and informative breakdown on a given cryptoasset while avoiding any controversial topics or contentious areas that might upset a team or community.
+
+## Questions & Concerns
+
+If you are reviewing this document due to what you believe is an incorrect or misleading piece of information in a cryptoasset write-up, we want to know! Please get in touch with our team using our [contact form](https://ark.dev/contact) and provide a detailed description of the text in which you take issue. In addition, please provide references for the correct information so that we can properly verify the information prior to making any update. Finally, if you include an email address, we will notify you of our decision and any updates we make.
