d06cfe05980d76ecf780efeacd81c3f91305eefa
Maurizio <leMaur@users.noreply.github.com>
2022-01-19 17:01:09 +0100

    refactor: update icon references (#222)

--- docs/marketsquare/support/supported-assets.md.blade.php
+++ docs/marketsquare/support/supported-assets.md.blade.php
@@ -18,23 +18,23 @@ Any cryptoasset listed on [CoinMarketCap](https://coinmarketcap.com) should also
 
 |                                                                       |                               Examples                                |                                                                       |
 | :-------------------------------------------------------------------: | :-------------------------------------------------------------------: | :-------------------------------------------------------------------: |
-| <x-ark-icon name="networks/ark" size="md" />          **ARK**         | <x-ark-icon name="networks/avalanche" size="md" />    **Avalanche**   | <x-ark-icon name="networks/hive" size="md" />         **Hive**        |
-| <x-ark-icon name="networks/binance" size="md" />      **Binance**     | <x-ark-icon name="networks/bitcoin" size="md" />      **Bitcoin**     | <x-ark-icon name="networks/cardano" size="md" />      **Cardano**     |
-| <x-ark-icon name="networks/cosmos" size="md" />       **Cosmos**      | <x-ark-icon name="networks/ethereum" size="md" />     **Ethereum**    | <x-ark-icon name="networks/eos" size="md" />          **EOS**         |
-| <x-ark-icon name="networks/neo" size="md" />          **NEO**         | <x-ark-icon name="networks/polkadot" size="md" />     **Polkadot**    | <x-ark-icon name="networks/tron" size="md" />         **Tron**        |
+| <x-ark-icon name="networks.ark" size="md" />          **ARK**         | <x-ark-icon name="networks.avalanche" size="md" />    **Avalanche**   | <x-ark-icon name="networks.hive" size="md" />         **Hive**        |
+| <x-ark-icon name="networks.binance" size="md" />      **Binance**     | <x-ark-icon name="networks.bitcoin" size="md" />      **Bitcoin**     | <x-ark-icon name="networks.cardano" size="md" />      **Cardano**     |
+| <x-ark-icon name="networks.cosmos" size="md" />       **Cosmos**      | <x-ark-icon name="networks.ethereum" size="md" />     **Ethereum**    | <x-ark-icon name="networks.eos" size="md" />          **EOS**         |
+| <x-ark-icon name="networks.neo" size="md" />          **NEO**         | <x-ark-icon name="networks.polkadot" size="md" />     **Polkadot**    | <x-ark-icon name="networks.tron" size="md" />         **Tron**        |
 
 ### Categories
 
 Categories supported for use by Projects on MarketSquare.
 
-|                                                                                   |                                     Supported                                     |                                                                                   |
-| :-------------------------------------------------------------------------------: | :-------------------------------------------------------------------------------: | :-------------------------------------------------------------------------------: |
-| <x-ark-icon name="business_registration" size="md" />     **Business**            | <x-ark-icon name="cards" size="md" />                     **Cards**               | <x-ark-icon name="collectibles" size="md" />              **Collectibles**        |
-| <x-ark-icon name="developer-tools" size="md" />           **Developer Tools**     | <x-ark-icon name="exchanges" size="md" />                 **Exchanges**           | <x-ark-icon name="finance" size="md" />                   **Finance**             |
-| <x-ark-icon name="controller" size="md" />                **Games**               | <x-ark-icon name="governance" size="md" />                **Governance**          | <x-ark-icon name="health-fitness" size="md" />            **Health & Fitness**    |
-| <x-ark-icon name="identity" size="md" />                  **Identity**            | <x-ark-icon name="organizations" size="md" />             **Organizations**       | <x-ark-icon name="security" size="md" />                  **Security**            |
-| <x-ark-icon name="social-media" size="md" />              **Social Media**        | <x-ark-icon name="storage" size="md" />                   **Storage**             | <x-ark-icon name="travel" size="md" />                    **Travel**              |
-| <x-ark-icon name="wallet" size="md" />                    **Wallets**             |                                                                                   |                                                                                   |
+|                                                                                           |                                     Supported                                     |                                                                                    |
+| :---------------------------------------------------------------------------------------: | :-------------------------------------------------------------------------------: | :--------------------------------------------------------------------------------: |
+| <x-ark-icon name="transaction.business-registration" size="md" />     **Business**        | <x-ark-icon name="cards" size="md" />                     **Cards**               | <x-ark-icon name="transaction.ipfs" size="md" />              **Collectibles**     |
+| <x-ark-icon name="developer-tools" size="md" />                       **Developer Tools** | <x-ark-icon name="exchanges" size="md" />                 **Exchanges**           | <x-ark-icon name="globe-wallet" size="md" />                  **Finance**          |
+| <x-ark-icon name="controller" size="md" />                            **Games**           | <x-ark-icon name="governance" size="md" />                **Governance**          | <x-ark-icon name="health-fitness" size="md" />                **Health & Fitness** |
+| <x-ark-icon name="identity" size="md" />                              **Identity**        | <x-ark-icon name="organizations" size="md" />             **Organizations**       | <x-ark-icon name="shield" size="md" />                        **Security**         |
+| <x-ark-icon name="social-media" size="md" />                          **Social Media**    | <x-ark-icon name="storage" size="md" />                   **Storage**             | <x-ark-icon name="semi-globe-sign" size="md" />               **Travel**           |
+| <x-ark-icon name="wallet" size="md" />                                **Wallets**         |                                                                                   |                                                                                    |
 
 ### Tags
 
@@ -80,7 +80,7 @@ Video sharing platforms supported for use by Projects on MarketSquare.
 
 |                               Supported                               |
 | :-------------------------------------------------------------------: |
-| <x-ark-icon name="brands/youtube" size="md" />        **Youtube**     |
+| <x-ark-icon name="brands.youtube" size="md" />        **Youtube**     |
 
 ### Social Media
 
@@ -88,10 +88,10 @@ Social Media platforms supported for use by Profiles and Projects on MarketSquar
 
 |                                                                       |                               Supported                               |                                                                       |
 | :-------------------------------------------------------------------: | :-------------------------------------------------------------------: | :-------------------------------------------------------------------: |
-| <x-ark-icon name="brands/discord" size="md" />        **Discord**     | <x-ark-icon name="brands/facebook" size="md" />       **Facebook**    | <x-ark-icon name="networks/hive" size="md" />         **Hive**        |
-| <x-ark-icon name="brands/instagram" size="md" />      **Instagram**   | <x-ark-icon name="brands/linkedin" size="md" />       **Linkedin**    | <x-ark-icon name="brands/medium" size="md" />         **Medium**      |
-| <x-ark-icon name="brands/reddit" size="md" />         **Reddit**      | <x-ark-icon name="brands/slack" size="md" />          **Slack**       | <x-ark-icon name="brands/telegram" size="md" />       **Telegram**    |
-| <x-ark-icon name="brands/twitter" size="md" />        **Twitter**     | <x-ark-icon name="brands/youtube" size="md" />        **Youtube**     | <x-ark-icon name="brands/outline/weibo" size="md" />  **Weibo**       |
+| <x-ark-icon name="brands.discord" size="md" />        **Discord**     | <x-ark-icon name="brands.facebook" size="md" />       **Facebook**    | <x-ark-icon name="networks/hive" size="md" />         **Hive**        |
+| <x-ark-icon name="brands.instagram" size="md" />      **Instagram**   | <x-ark-icon name="brands.linkedin" size="md" />       **Linkedin**    | <x-ark-icon name="brands.medium" size="md" />         **Medium**      |
+| <x-ark-icon name="brands.reddit" size="md" />         **Reddit**      | <x-ark-icon name="brands.slack" size="md" />          **Slack**       | <x-ark-icon name="brands.telegram" size="md" />       **Telegram**    |
+| <x-ark-icon name="brands.twitter" size="md" />        **Twitter**     | <x-ark-icon name="brands.youtube" size="md" />        **Youtube**     | <x-ark-icon name="brands.outline/weibo" size="md" />  **Weibo**       |
 
 ### Repositories
 
@@ -99,4 +99,4 @@ Repositories supported for use by Profiles and Projects on MarketSquare.
 
 |                                                                       |                               Supported                               |                                                                       |
 | :-------------------------------------------------------------------: | :-------------------------------------------------------------------: | :-------------------------------------------------------------------: |
-| <x-ark-icon name="brands/bitbucket" size="md" />      **BitBucket**   | <x-ark-icon name="brands/github" size="md" />         **Github**      | <x-ark-icon name="brands/gitlab" size="md" />         **GitLab**      |
+| <x-ark-icon name="brands.bitbucket" size="md" />      **BitBucket**   | <x-ark-icon name="brands.github" size="md" />         **Github**      | <x-ark-icon name="brands.gitlab" size="md" />         **GitLab**      |
