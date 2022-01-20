a33bd36dd4c2771945deebe410af1c480993f03f
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2022-01-20 13:04:07 -0500

    chore(nodem): finish teams page
    - update steps
    - add username and role descriptors
    - add images

--- docs/nodem/usage/teams.md.blade.php
+++ docs/nodem/usage/teams.md.blade.php
@@ -10,17 +10,42 @@ After [setting up your owner account](/docs/nodem/usage/owner), you can log in a
 
 Inviting users is a straightforward process outlined below.
 
-1) Get started by clicking `profile` and then `team`
-2) Fill in the details of the person you want to invite
-3) Click `create` to generate your new member's invitation code
-4) Share this invitation code with your new member who will use this code to sign up
+1) Get started by clicking on your profile's avatar, then select **'team'** from the dropdown menu.
 
-<!--
-TODO: add images
--->
+![](/storage/docs/docs/nodem/assets/usage/teams-menu-select.png)
 
-## Team Member Roles
+2) Click the **'Invite'** button on the Team Management Dashboard.
 
-<!--
-TODO: list roles and permissions.
--->
+![](/storage/docs/docs/nodem/assets/usage/teams-invite-button.png)
+
+3) Fill in the details of the person you want to invite.
+
+![](/storage/docs/docs/nodem/assets/usage/teams-invite-details.png)
+
+* **Username** - This will be used by your new team member at sign-up and to sign-in to Nodem
+  * It visible in the Team Management Dashboard
+  * It cannot be changed. If a new username is desired, the team member account must be removed, and a new account must be created
+  * Team Members may be removed, and 'Roles' may be reassigned by those with the appropriate permissions (i.e., Admins)
+* **Roles** - Assignable permissions that decide which actions may be performed by a team member
+  * **Readonly**
+    * Permitted to view servers and team members
+    * Permitted to change their password or configure 2FA
+    * May _not_ perform basic server actions (e.g., start, stop, restart, etc.)
+    * May _not_ add, edit, or remove servers.
+    * May _not_ add, edit, or remove team members
+  * **Maintainer**
+    * All 'Readonly' permissions
+    * Permitted to perform basic server actions (e.g., start, stop, restart, etc.)
+    * Permitted to add, edit, or remove servers.
+  * **Admin**
+    * All 'Maintainer' permissions
+    * Permitted to perform all server actions
+    * Permitted to add, edit, or remove team members, including the reassignment of roles
+
+4) Click 'Generate' to create your new member's invitation code.
+
+![](/storage/docs/docs/nodem/assets/usage/teams-invite-generate.png)
+
+5) Share this invitation code with your new member who will use it at sign-up.
+
+![](/storage/docs/docs/nodem/assets/usage/teams-invite-code.png)
