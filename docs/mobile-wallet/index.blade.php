1ee11288c0506e7d12edee6e6740b0047adf82de
Josip Crnković <jcrnkovic95@gmail.com>
2022-03-28 13:45:10 +0200

    Mobile Wallet

---
+++ docs/mobile-wallet/index.blade.php
@@ -0,0 +1,11 @@
+<x-general.sidebar-group path="/docs/mobile-wallet/intro" title="Introduction">
+    <x-general.sidebar-link path="/docs/mobile-wallet/intro" name="Introduction" />
+</x-general.sidebar-group>
+
+<x-general.sidebar-group path="/docs/mobile-wallet/user-guides/*" title="Getting Started">
+    <x-general.sidebar-link path="/docs/mobile-wallet/user-guides/installation" name="Installation" />
+    <x-general.sidebar-link path="/docs/mobile-wallet/user-guides/starting-the-wallet" name="Starting the Wallet" />
+    <x-general.sidebar-link path="/docs/mobile-wallet/user-guides/navigating-the-mobile-wallet-interface" name="Wallet Navigation" />
+    <x-general.sidebar-link path="/docs/mobile-wallet/user-guides/how-to-create-or-import-a-wallet" name="Create or Import a Wallet" />
+    <x-general.sidebar-link path="/docs/mobile-wallet/user-guides/how-to-vote-for-a-delegate" name="Vote for a Delegate" />
+</x-general.sidebar-group>
