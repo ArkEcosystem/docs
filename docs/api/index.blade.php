a2169fe49cf4570be6f9900c1e453c4f2abb0efd
Josip Crnković <jcrnkovic95@gmail.com>
2022-03-28 13:44:31 +0200

    API

---
+++ docs/api/index.blade.php
@@ -0,0 +1,48 @@
+<x-general.sidebar-group path="/docs/api/intro" title="Introduction">
+    <x-general.sidebar-link path="/docs/api/intro" name="Introduction" />
+</x-general.sidebar-group>
+
+<x-general.sidebar-group path="/docs/api/public-rest-api/*" title="Public REST API">
+    <x-general.sidebar-link path="/docs/api/public-rest-api/getting-started" name="Getting Started" />
+    <x-general.sidebar-link
+        path="/docs/api/public-rest-api/endpoints"
+        name="Endpoints"
+        :children="[
+            ['path' => '/docs/api/public-rest-api/endpoints/blockchain', 'name' => 'Blockchain'],
+            ['path' => '/docs/api/public-rest-api/endpoints/blocks', 'name' => 'Blocks'],
+            ['path' => '/docs/api/public-rest-api/endpoints/delegates', 'name' => 'Delegates'],
+            ['path' => '/docs/api/public-rest-api/endpoints/entities', 'name' => 'Entities'],
+            ['path' => '/docs/api/public-rest-api/endpoints/node', 'name' => 'Node'],
+            ['path' => '/docs/api/public-rest-api/endpoints/peers', 'name' => 'Peers'],
+            ['path' => '/docs/api/public-rest-api/endpoints/transactions', 'name' => 'Transactions'],
+            ['path' => '/docs/api/public-rest-api/endpoints/votes', 'name' => 'Votes'],
+            ['path' => '/docs/api/public-rest-api/endpoints/locks', 'name' => 'Locks'],
+            ['path' => '/docs/api/public-rest-api/endpoints/wallets', 'name' => 'Wallets'],
+        ]"
+    />
+    <x-general.sidebar-link path="/docs/api/public-rest-api/modify-response" name="Modify response" />
+    <x-general.sidebar-link path="/docs/api/public-rest-api/troubleshooting" name="Troubleshooting" />
+</x-general.sidebar-group>
+
+
+<x-general.sidebar-group path="/docs/api/webhook-api/*" title="Webhook API">
+    <x-general.sidebar-link path="/docs/api/webhook-api/getting-started" name="Getting Started" />
+    <x-general.sidebar-link path="/docs/api/webhook-api/usage" name="Usage" />
+    <x-general.sidebar-link path="/docs/api/webhook-api/endpoints" name="Endpoints" />
+</x-general.sidebar-group>
+
+<x-general.sidebar-group path="/docs/api/manager-api/*" title="Manager API">
+    <x-general.sidebar-link path="/docs/api/manager-api/getting-started" name="Getting Started" />
+    <x-general.sidebar-link
+        path="/docs/api/manager-api/endpoints"
+        name="Endpoints"
+        :children="[
+            ['path' => '/docs/api/manager-api/endpoints/configuration', 'name' => 'Configuration'],
+            ['path' => '/docs/api/manager-api/endpoints/info', 'name' => 'Info'],
+            ['path' => '/docs/api/manager-api/endpoints/log', 'name' => 'Log'],
+            ['path' => '/docs/api/manager-api/endpoints/process', 'name' => 'Process'],
+            ['path' => '/docs/api/manager-api/endpoints/snapshots', 'name' => 'Snapshots'],
+            ['path' => '/docs/api/manager-api/endpoints/watcher', 'name' => 'Watcher'],
+        ]"
+    />
+</x-general.sidebar-group>
