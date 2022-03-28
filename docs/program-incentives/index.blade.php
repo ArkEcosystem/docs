94ccb9992bfcc1dc4c8cb2b10235455f4b2dfd0f
Josip Crnković <jcrnkovic95@gmail.com>
2022-03-28 13:45:28 +0200

    Program Incentives

---
+++ docs/program-incentives/index.blade.php
@@ -0,0 +1,24 @@
+<x-general.sidebar-group path="/docs/program-incentives/intro" title="Introduction">
+    <x-general.sidebar-link path="/docs/program-incentives/intro" name="Introduction" />
+</x-general.sidebar-group>
+
+<x-general.sidebar-group :path="[
+    '/docs/program-incentives/ark-grants-program-guidelines',
+    '/docs/program-incentives/development-bounty-program',
+    '/docs/program-incentives/security-vulnerability-program',
+]" title="Programs">
+    <x-general.sidebar-link path="/docs/program-incentives/ark-grants-program-guidelines" name="ARK Grants" />
+    <x-general.sidebar-link path="/docs/program-incentives/development-bounty-program" name="Development Bounty" />
+    <x-general.sidebar-link path="/docs/program-incentives/security-vulnerability-program" name="Security Vulnerability" />
+</x-general.sidebar-group>
+
+<x-general.sidebar-group path="/docs/program-incentives/guidelines/*" title="Guidelines">
+    <x-general.sidebar-link path="/docs/program-incentives/guidelines/contributing" name="Contributing" />
+    <x-general.sidebar-link path="/docs/program-incentives/guidelines/clean-code-and-tests" name="Clean Code & Tests" />
+    <x-general.sidebar-link path="/docs/program-incentives/guidelines/version-control" name="Version Control" />
+    <x-general.sidebar-link path="/docs/program-incentives/guidelines/releases-and-versioning" name="Releases & Versioning" />
+    <x-general.sidebar-link path="/docs/program-incentives/guidelines/code-reviews" name="Code Reviews" />
+    <x-general.sidebar-link path="/docs/program-incentives/guidelines/laravel-and-php" name="Laravel & PHP" />
+    <x-general.sidebar-link path="/docs/program-incentives/guidelines/frontend-frameworks" name="Frontend Frameworks" />
+    <x-general.sidebar-link path="/docs/program-incentives/guidelines/going-live" name="Going Live!" />
+</x-general.sidebar-group>
