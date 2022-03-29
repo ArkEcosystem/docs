ef058266791093055b4b57a2b5a7adfeee583f81
Josip Crnković <jcrnkovic95@gmail.com>
2022-03-29 10:29:03 +0200

    SDK

---
+++ docs/sdk/index.blade.php
@@ -0,0 +1,33 @@
+<x-general.sidebar-group path="/docs/sdk/intro" title="Introduction">
+    <x-general.sidebar-link path="/docs/sdk/intro" name="Introduction" />
+</x-general.sidebar-group>
+
+<x-general.sidebar-group path="/docs/sdk/guidelines/*" title="Guidelines">
+    <x-general.sidebar-link path="/docs/sdk/guidelines/crypto" name="Crypto Libraries" />
+    <x-general.sidebar-link path="/docs/sdk/guidelines/client" name="Client Libraries" />
+</x-general.sidebar-group>
+
+@foreach(Documentation::availableSDK() as $sdk)
+<x-sdk-sidebar-group
+    :heading="2"
+    :name="$sdk['name']"
+    :slug="$sdk['slug']"
+    :has-complementary="$sdk['has_complementary']"
+/>
+@endforeach
+
+<h3 class="documentation-sidebar-subheading">Deprecated</h3>
+
+@foreach(Documentation::deprecatedSDK() as $sdk)
+<x-sdk-sidebar-group
+    :heading="3"
+    :name="$sdk['name']"
+    :slug="$sdk['slug']"
+    :has-complementary="$sdk['has_complementary']"
+/>
+@endforeach
+
+<x-general.sidebar-group path="/docs/sdk/frameworks/*" title="Frameworks">
+    <x-general.sidebar-link path="/docs/sdk/frameworks/laravel" name="Laravel" />
+    <x-general.sidebar-link path="/docs/sdk/frameworks/symfony" name="Symfony" />
+</x-general.sidebar-group>
