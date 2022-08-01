19ff286a15caf2ce616bca4a50a181404c761055
alfonsobries <alfonsobries@gmail.com>
2022-08-01 15:34:16 -0500

    remove border on tutorials

--- tutorials/ark-messenger-tutorial/index.blade.php
+++ tutorials/ark-messenger-tutorial/index.blade.php
@@ -1,4 +1,4 @@
-<x-ark-docs-sidebar-link top-level path="/tutorials/ark-messenger-tutorial" name="Introduction" />
-<x-ark-docs-sidebar-link top-level path="/tutorials/ark-messenger-tutorial/part-one" name="Part One" />
-<x-ark-docs-sidebar-link top-level path="/tutorials/ark-messenger-tutorial/part-two" name="Part Two" />
-<x-ark-docs-sidebar-link top-level path="/tutorials/ark-messenger-tutorial/part-three" name="Part Three" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/ark-messenger-tutorial" name="Introduction" :top-border="false" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/ark-messenger-tutorial/part-one" name="Part One" :top-border="false" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/ark-messenger-tutorial/part-two" name="Part Two" :top-border="false" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/ark-messenger-tutorial/part-three" name="Part Three" :top-border="false" />
--- tutorials/launching-html5-games-in-the-ark-desktop-wallet/index.blade.php
+++ tutorials/launching-html5-games-in-the-ark-desktop-wallet/index.blade.php
@@ -1,7 +1,7 @@
-<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet" name="Introduction" />
-<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-one" name="Part One" />
-<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-two" name="Part Two" />
-<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-three" name="Part Three" />
-<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-four" name="Part Four" />
-<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-five" name="Part Five" />
-<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-six" name="Part Six" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet" name="Introduction" :top-border="false" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-one" name="Part One" :top-border="false" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-two" name="Part Two" :top-border="false" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-three" name="Part Three" :top-border="false" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-four" name="Part Four" :top-border="false" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-five" name="Part Five" :top-border="false" />
+<x-ark-docs-sidebar-link top-level path="/tutorials/launching-html5-games-in-the-ark-desktop-wallet/part-six" name="Part Six" :top-border="false" />
