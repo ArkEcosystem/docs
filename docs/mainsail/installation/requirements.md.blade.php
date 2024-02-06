ecfe5612a653051c20169d49aa363947a283762c
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-06 17:40:02 +0100

    Requirements

--- docs/mainsail/installation/requirements.md.blade.php
+++ docs/mainsail/installation/requirements.md.blade.php
@@ -5,29 +5,16 @@ title: Installation - Server Requirements
 # Server Requirements
 
 <x-alert type="success">
-If you don't have access to a Linux box you can quickly setup one on [DigitalOcean](https://cloud.digitalocean.com) by using this **$100** referral link: [Referral Link](https://m.do.co/c/09d061526b12).
+If you don't have access to a Linux box you can quickly setup one on [DigitalOcean](https://cloud.digitalocean.com) by using this **$w00** referral link: [Referral Link](https://m.do.co/c/09d061526b12).
 </x-alert>
 
----
-
-<x-alert type="warning">
-Ubuntu 16.x is **no longer** supported. We also strongly suggest using PostgreSQL 15.x for Core when setting up a new server.
-</x-alert>
-
----
 
 | Supported OS | Release Version(s) |
 | :----------: | :----------------: |
-| **Ubuntu**   | 18.x / 20.x / 22.x       |
+| **Ubuntu**   | 22.x       |
 
-| Relay Specification | Minimum | Recommended   |
+| Node Specification | Minimum | Recommended   |
 | :-----------------: | :-----: | :-----------: |
 | **CPUs**            | 2       | 4             |
 | **RAM**             | 4GB     | 8GB           |
-| **HDD**             | 80GB    | 100GB - 120GB |
-
-| Forger Specification | Minimum            | Recommended              |
-| :------------------: | :----------------: | :----------------------: |
-| **CPUs**             | 2<br>(_dedicated_) | 4<br>(_dedicated_)       |
-| **RAM**              | 8GB                | 16GB                     |
-| **HDD**              | 80GB<br>(_SSD_)    | 100GB - 120GB<br>(_SSD_) |
+| **HDD**             | 20GB    | 20GB - 100GB |
