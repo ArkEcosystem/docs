2b113fac68f2ce30ce8a935f34b4136e6679d62f
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-06 16:14:43 +0100

    Container

--- docs/mainsail/overview/architecture/container.md.blade.php
+++ docs/mainsail/overview/architecture/container.md.blade.php
@@ -4,7 +4,7 @@ title: Architecture - Service Container
 
 # Service Container
 
-The ARK Core service container is responsible for managing bindings, dependencies and dependency injection. Our container of choice is [InversifyJS](https://github.com/inversify/InversifyJS/) which describes itself as `A powerful and lightweight inversion of control container for JavaScript & Node.js apps powered by TypeScript`.
+The Mainsail service container is responsible for managing bindings, dependencies and dependency injection. Our container of choice is [InversifyJS](https://github.com/inversify/InversifyJS/) which describes itself as `A powerful and lightweight inversion of control container for JavaScript & Node.js apps powered by TypeScript`.
 
 An IoC container uses a class constructor to identify and inject its dependencies. InversifyJS has a friendly API and encourages the usage of the best OOP and IoC practices. The wide variety of features it provides combined with its strictness make it a perfect choice for Core where type safety is important.
 
@@ -17,10 +17,10 @@ The service container provides various methods of binding values to the containe
 A constant value is any value that does not change after its creation. **It should be bound once to the container and not be changed.**
 
 ```typescript
-import { app, Container, Contracts } from "@arkecosystem/core-kernel";
+import { Contracts, Identifiers } from "@mainsail/contracts";
 
 app
-    .bind<Contracts.Kernel.Log.Logger>(Container.Identifiers.LogService)
+    .bind<Identifiers.Services.Log.Service>(Contracts.Kernel.Logger)
     .toConstantValue(new ConsoleLogger());
 ```
 
@@ -29,10 +29,10 @@ app
 A dynamic value is any value that is the result of meeting a condition to be created or calling another function and returning its value.
 
 ```typescript
-import { app, Container, Contracts } from "@arkecosystem/core-kernel";
+import { Contracts, Identifiers } from "@mainsail/contracts";
 
 app
-    .bind<Contracts.Kernel.Log.Logger>(Container.Identifiers.LogService)
+    .bind<Contracts.Kernel.Log.Logger>(Contracts.Kernel.Logger)
     .toDynamicValue((context: interfaces.Context) => { return new ConsoleLogger(); });
 ```
 
@@ -43,10 +43,10 @@ app
 An implementation bound to a contract is useful in cases where you wish to make use of dependency injection. You'll bind your `ConsoleLogger` implementation to the `ILogger` contract and InversifyJS will take care of returning an instance of the `ConsoleLogger` implementation when the `ILogger` contract is resolved.
 
 ```typescript
-import { app, Container, Contracts } from "@arkecosystem/core-kernel";
+import { Contracts, Identifiers } from "@mainsail/contracts";
 
 app
-    .bind<Contracts.Kernel.Log.Logger>(Container.Identifiers.LogService)
+    .bind<Contracts.Kernel.Log.Logger>(Contracts.Kernel.Logger)
     .to(ConsoleLogger);
 ```
 
@@ -59,9 +59,9 @@ Now that we know how to create bindings we also need a way of resolving them fro
 The `get` method can be used to resolve any previously created binding from the container. If the binding with the given name doesn't exist an exception will be thrown.
 
 ```typescript
-import { app, Container, Contracts } from "@arkecosystem/core-kernel";
+import { Contracts, Identifiers } from "@mainsail/contracts";
 
-app.get<Contracts.Kernel.Log.Logger>(Container.Identifiers.LogService);
+app.get<Contracts.Kernel.Logger>(Contracts.Kernel.Logger);
 ```
 
 ### Resolving Contracts to Implementations
@@ -69,9 +69,9 @@ app.get<Contracts.Kernel.Log.Logger>(Container.Identifiers.LogService);
 The `resolve` method can be used if you want to get an instance of a class that depends on dependency injection.
 
 ```typescript
-import { app, Contracts } from "@arkecosystem/core-kernel";
+import { Contracts, Identifiers } from "@mainsail/contracts";
 
-app.resolve<Contracts.Kernel.Log.Logger>(ConsoleLogger);
+app.resolve<Contracts.Kernel.Logger>(ConsoleLogger);
 ```
 
 > You should rarely find yourself calling this, if you do end up in a situation where you call it for basically everything you should rethink the architecture of you plugin and how you interact with the container.
