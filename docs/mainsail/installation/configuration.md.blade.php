6abdee99f9eb31e8997faf26fc97d72085401502
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-06 18:02:40 +0100

    Configuration

--- docs/mainsail/installation/configuration.md.blade.php
+++ docs/mainsail/installation/configuration.md.blade.php
@@ -1,110 +1,82 @@
 ---
-title: Installation - Configuring Core
+title: Installation - Configuring Mainsail
 ---
 
-# Configuring Core
+# Configuring Mainsail
 
-ARK Core provides a wide variety of ways to configure how it behaves, what plugins are used, who is forging and more. Each option is documented, so head over to the [ARK Core repository](https://github.com/ArkEcosystem/core/) and dive in. All of this configuration can be found in the `app.json`, `delegates.json` and `peers.json` files of the respective network you are using.
+Mainsail provides a wide variety of ways to configure how it behaves, what plugins are used, who is forging and more. Each option is documented, so head over to the [Mainsail repository](https://github.com/ArkEcosystem/mainsail) and [Mainsail Network Config repository](https://github.com/ArkEcosystem/mainsail-network-config/tree/main/testnet/mainsail). All of this configuration can be found in the `app.json`, `crypto.json`, `validators.json` and `peers.json` files of the respective network you are using.
 
 ## Plugins Configuration
 
-Plugins are packages that expose services which expand or alter what Core is capable of doing, by default there are a few required plugins like the a database driver, transaction pool, peer communication and consensus.
+Plugins are packages that expose services which expand or alter what Mainsail node is capable of doing, by default there are a few required plugins like the a database driver, transaction pool, peer communication and consensus.
 
 ```json
 {
-    "core": {
-        "plugins": [
-            {
-                "package": "@arkecosystem/core-logger-pino"
-            },
-            {
-                "package": "@arkecosystem/core-state"
-            },
-            {
-                "package": "@arkecosystem/core-database"
-            },
-            {
-                "package": "@arkecosystem/core-transactions"
-            },
-            {
-                "package": "@arkecosystem/core-magistrate-transactions"
-            },
-            {
-                "package": "@arkecosystem/core-transaction-pool"
-            },
-            {
-                "package": "@arkecosystem/core-p2p"
-            },
-            {
-                "package": "@arkecosystem/core-blockchain"
-            },
-            {
-                "package": "@arkecosystem/core-api"
-            },
-            {
-                "package": "@arkecosystem/core-magistrate-api"
-            },
-            {
-                "package": "@arkecosystem/core-webhooks"
-            },
-            {
-                "package": "@arkecosystem/core-forger"
-            }
-        ]
-    },
-    "relay": {
-        "plugins": [
-            {
-                "package": "@arkecosystem/core-logger-pino"
-            },
-            {
-                "package": "@arkecosystem/core-state"
-            },
-            {
-                "package": "@arkecosystem/core-database"
-            },
-            {
-                "package": "@arkecosystem/core-transactions"
-            },
-            {
-                "package": "@arkecosystem/core-magistrate-transactions"
-            },
-            {
-                "package": "@arkecosystem/core-transaction-pool"
-            },
-            {
-                "package": "@arkecosystem/core-p2p"
-            },
-            {
-                "package": "@arkecosystem/core-blockchain"
-            },
-            {
-                "package": "@arkecosystem/core-api"
-            },
-            {
-                "package": "@arkecosystem/core-magistrate-api"
-            },
-            {
-                "package": "@arkecosystem/core-webhooks"
-            }
-        ]
-    },
-    "forger": {
-        "plugins": [
-            {
-                "package": "@arkecosystem/core-logger-pino"
-            },
-            {
-                "package": "@arkecosystem/core-forger"
-            }
-        ]
-    }
+    "plugins": [
+		{
+			"package": "@mainsail/validation"
+		},
+		{
+			"package": "@mainsail/crypto-config"
+		},
+		{
+			"package": "@mainsail/crypto-validation"
+		},
+		{
+			"package": "@mainsail/crypto-hash-bcrypto"
+		},
+		{
+			"package": "@mainsail/crypto-signature-schnorr-secp256k1"
+		},
+		{
+			"package": "@mainsail/crypto-key-pair-ecdsa"
+		},
+		{
+			"package": "@mainsail/crypto-consensus-bls12-381"
+		},
+		{
+			"package": "@mainsail/crypto-address-base58"
+		},
+		{
+			"package": "@mainsail/crypto-wif"
+		},
+		{
+			"package": "@mainsail/serializer"
+		},
+		{
+			"package": "@mainsail/p2p"
+		},
+		{
+			"package": "@mainsail/api-transaction-pool"
+		},
+		{
+			"package": "@mainsail/processor"
+		},
+		{
+			"package": "@mainsail/validator-set-static"
+		},
+		{
+			"package": "@mainsail/validator"
+		},
+		{
+			"package": "@mainsail/proposer"
+		},
+		{
+			"package": "@mainsail/consensus"
+		},
+		{
+			"package": "@mainsail/webhooks"
+		},
+		{
+			"package": "@mainsail/bootstrap"
+		}
+	]
 }
 ```
 
 Again the configuration is quite simple. We have a `plugins` key that serves as a namespace for all of our plugin specific configuration, for each process type. Then we see a list of objects that all contain `package` key and an optional `options` key.
 
-The `package` key is used by Core to decide what package should be loaded, this can be either an npm package name or path to a local package.
+The `package` key is used by Mainsail to decide what package should be loaded, this can be either an npm package name or path to a local package.
 
 ### Plugin options
 
@@ -114,7 +86,7 @@ Keys that are type of object will be merged with default configuration. String, 
 
 ```json
 {
-    "package": "@arkecosystem/core-logger-pino",
+    "package": "@mainsail/logger-pino",
     "options": {
         "fileRotator": {
             "interval": "2d"
@@ -123,42 +95,44 @@ Keys that are type of object will be merged with default configuration. String, 
 },
 ```
 
-## Delegate Configuration
+## Validator Configuration
 
-The delegate configuration is responsible for providing the information that is necessary to know who should be forging on a server. This can either be a list of plaintext bip39 passphrases or a bip38 encrypted passphrase that is protected with a password.
+The validator configuration is responsible for providing the information that is necessary to know who should be forging on a server. This can either be a list of plaintext bip39 passphrases or a bip38 encrypted passphrase that is protected with a password. Configuration is stored inside `validators.json`ß file.
+
+```json
+{
+    "secrets": [
+        "validator mnemonic"
+    ]
+}
+```
 
 ## Peer Configuration
 
-The peer configuration is responsible for providing the information that is necessary to know who the seeds are so that we can retrieve a list of peers from them and start communicating with the network. By default it ships with a list of seeds and an additional list of sources on GitHub which is loaded when Core starts.
+The peer configuration is responsible for providing the information that is necessary to know who the seeds are so that we can retrieve a list of peers from them and start communicating with the network. By default it ships with a list of seeds and an additional list of sources on GitHub which is loaded when Core starts. Configuration is stored in `peers.json` file. 
 
 ## Environment Configuration
 
 Sometimes it can be beneficial or even necessary to have different configuration values per environment you are working with. An example would be where you are testing a new feature to improve database query or caching performance, in those cases you don't want to be bound to the settings of the production network but run with your own values and modifications.
 
 ```ini
-LOG_LEVEL=debug
-LOG_LEVEL_FILE=debug
+CORE_LOG_LEVEL=info
+CORE_LOG_LEVEL_FILE=debug
 
-DB_HOST=localhost
-DB_PORT=5432
-DB_DATABASE=ark_mainnet
-DB_USERNAME=ark
-DB_PASSWORD=password
+CORE_P2P_HOST=0.0.0.0
+CORE_P2P_PORT=4000
 
-P2P_HOST=0.0.0.0
-P2P_PORT=4000
+CORE_API_HOST=0.0.0.0
+CORE_API_PORT=4003
 
-WEBHOOKS_HOST=0.0.0.0
-WEBHOOKS_PORT=4004
-
-API_HOST=0.0.0.0
-API_PORT=4003
+CORE_WEBHOOKS_HOST=0.0.0.0
+CORE_WEBHOOKS_PORT=4004
 ```
 
 <x-alert type="warning">
-Your changes to the .env file should not be committed to your git repository, since each server could require a different environment configuration.
+Your changes to the `.env` file should not be committed to your git repository, since each server could require a different environment configuration.
 </x-alert>
 
-Those are just a few of the possible environment variables, check the [Environment Variables](/docs/mainsail/installation/variables) to get a full list of available environment variables and make sure to check out [dotenv](https://github.com/ArkEcosystem/utils/blob/master/src/dot-env.ts) and [sindresorhus/env-paths](https://github.com/sindresorhus/env-paths) to get a better understanding of what is happening under the hood.
+Those are just a few of the possible environment variables, check the [Environment Variables](/docs/mainsail/installation/variables) to get a full list of available environment variables and make sure to check out [dotenv](https://github.com/ArkEcosystem/mainsail/blob/develop/packages/utils/source/dot-env.ts) and [sindresorhus/env-paths](https://github.com/sindresorhus/env-paths) to get a better understanding of what is happening under the hood.
 
 > Your changes to the `.env` file should not be committed to your git repository, since each server could require a different environment configuration.
