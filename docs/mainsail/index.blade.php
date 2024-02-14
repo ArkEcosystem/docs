e5e385d642ba834e1df653994d230a4e1eca3c4d
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-14 11:50:30 +0100

    Add links

--- docs/mainsail/index.blade.php
+++ docs/mainsail/index.blade.php
@@ -79,6 +79,7 @@
 <x-ark-docs-sidebar-group path="/docs/mainsail/deployment/*" title="Deployment">
     <x-ark-docs-sidebar-link path="/docs/mainsail/deployment/modes" name="Core Run Modes" />
     <x-ark-docs-sidebar-link path="/docs/mainsail/deployment/cli" name="Mainsail CLI Commands" />
+    <x-ark-docs-sidebar-link path="/docs/mainsail/deployment/cli-api" name="Mainsail API CLI Commands" />
     <x-ark-docs-sidebar-link path="/docs/mainsail/deployment/forger" name="Starting a Forger" />
 </x-ark-docs-sidebar-group>
 
--- docs/mainsail/intro.md.blade.php
+++ docs/mainsail/intro.md.blade.php
@@ -118,6 +118,7 @@ The **Deployment** section will guide you through important actions such as the 
     :links="[
         ['path' => '/docs/mainsail/deployment/modes', 'name' => 'Core Run Modes'],
         ['path' => '/docs/mainsail/deployment/cli', 'name' => 'Mainsail CLI Commands'],
+        ['path' => '/docs/mainsail/deployment/cli-api', 'name' => 'Mainsail API CLI Commands'],
         ['path' => '/docs/mainsail/deployment/forger', 'name' => 'Starting a Forger'],
     ]"
 />
