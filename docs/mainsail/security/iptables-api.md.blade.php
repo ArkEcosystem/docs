041cc197fa80e99f6f74b7de930987d26a134771
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-16 16:44:23 +0100

    Add api iptables

--- docs/mainsail/security/iptables-api.md.blade.php
+++ docs/mainsail/security/iptables-api.md.blade.php
@@ -2,7 +2,7 @@
 title: Security - Applying iptables
 ---
 
-# Applying P2P iptables
+# Applying API iptables
 
 iptables is a utility that allows system admins to configure IP packet filtering rules for Linux's kernel firewall. These rules are organized in separate tables and determine how to treat network packets. Using iptables is _highly recommended_ and shouldn't be overlooked unless required by your system's architecture and/or you're aware of the security implications.
 
@@ -17,31 +17,31 @@ Specifically:
 
 ## Script Usage
 
-| Command     | Execution                     |
+| Command     | Example                     |
 | :---------- | :---------------------------- |
-| **start**   | `bash p2p-iptables.sh start`   |
-| **stop**    | `bash p2p-iptables.sh stop`    |
-| **restart** | `bash p2p-iptables.sh restart` |
-| **status**  | `bash p2p-iptables.sh status`  |
+| **start**   | `bash api-iptables.sh start pool`   |
+| **stop**    | `bash api-iptables.sh stop pool`    |
+| **restart** | `bash api-iptables.sh restart pool` |
+| **status**  | `bash api-iptables.sh status pool`  |
 
 ## Running the Script
 
 Download and execute the iptables script using the following commands:
 
 ```shell
-wget -N https://raw.githubusercontent.com/ArkEcosystem/mainsail/main/scripts/p2p-iptables.sh
+wget -N https://raw.githubusercontent.com/ArkEcosystem/mainsail/main/scripts/api-iptables.sh
 ```
 
 Edit the iptables script an use correct settings. 
 
 ```shell
-nano ./p2p-iptables.sh
+nano ./api-iptables.sh
 ```
 
 Enable iptables:
 
 ```shell
-bash ./p2p-iptables.sh start
+bash ./api-iptables.sh start
 ```
 
 ## Configuration
@@ -49,15 +49,23 @@ bash ./p2p-iptables.sh start
 Edit the iptables script an review setting. 
 
 ```shell
-nano ./p2p-iptables.sh
+nano ./api-iptables.sh
 ```
-
 
 | Option     | Description                     |
 | :---------- | :---------------------------- |
-| P2P_GLOBAL   | Provide correct p2p PORT number. Correct setting might be different than default value.  |
-| P2P_GLOBAL_CONN    |  Number of connection when the 4 connections per 30 seconds limit is activated.   |
-| DOCKER | Set to `true` if docker image is used. |
+| API_GLOBAL   | Public API port number.  |
+| API_GLOBAL_RATE    |  Request rate for Public API.   |
+| API_GLOBAL_BURST | Burst for Public API. |
+| API_TRANSACTION_POOL_GLOBAL   | Transaction pool API port number.  |
+| API_GLOBAL_RATE    |  Request rate for Transaction pool.   |
+| API_GLOBAL_BURST | Burst for Transaction pool API. |
+
+| Procees     | Description                     |
+| :---------- | :---------------------------- |
+| api   | The public API that is part of `mainsail-api` script.  |
+| pool  |  The transaction pool API that is part of `mainsail` script.   |
+
 
 ## Creating a cron job
 
@@ -72,7 +80,8 @@ Because the filtering initiated by the iptables script does not persist after a 
 2. add the following line to the _**end**_ of the crontab file:
 
    ```shell
-   @reboot bash ~/p2p-iptables.sh start
+   @reboot bash ~/api-iptables.sh start pool
+   @reboot bash ~/api-iptables.sh start api
    ```
 
 3. save the changes and exit:
@@ -86,7 +95,7 @@ Because the filtering initiated by the iptables script does not persist after a 
 4. apply the permissions:
 
    ```shell
-   sudo bash -c "echo \"$USER ALL=(ALL) NOPASSWD:/sbin/p2p-iptables\" >> /etc/sudoers"
+   sudo bash -c "echo \"$USER ALL=(ALL) NOPASSWD:/sbin/api-iptables\" >> /etc/sudoers"
    ```
 
 <x-alert type="warning">
