5b486907e08fc1a5387c0f2f95fe1988f767ca66
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-16 17:12:56 +0100

    Add networks

---
+++ docs/mainsail/installation/networks.md.blade.php
@@ -0,0 +1,30 @@
+---
+title: Installation - Networks
+---
+
+# Networks
+
+<x-alert type="success">
+If you don't have access to a Linux box you can quickly setup one on [DigitalOcean](https://cloud.digitalocean.com) by using this **$w00** referral link: [Referral Link](https://m.do.co/c/09d061526b12).
+</x-alert>
+
+Active Mainsail networks:
+
+## Testnet (ARK)
+
+### Files
+
+| Name | File |
+| :----------: | :----------------: |
+| **Crypto**   | [crypto.json](https://raw.githubusercontent.com/ArkEcosystem/mainsail-network-config/main/testnet/mainsail/crypto.json)  |
+| **App**   | [app.json](https://raw.githubusercontent.com/ArkEcosystem/mainsail-network-config/main/testnet/mainsail/app.json)  |
+| **Seed peers**   | [peers.json](https://raw.githubusercontent.com/ArkEcosystem/mainsail-network-config/main/testnet/mainsail/peers.json)  |
+
+### Ports
+
+| Process                    | Port number |
+| :------------------------: | :---------: |
+| **P2P**                    | 4000        |
+| **Public API**             | 4003        |
+| **Transaction Pool API**   | 4007        |
+
