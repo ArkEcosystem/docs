ff59b2740c6bb01ee7f3033c91f123787f575df9
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2024-03-06 10:58:02 +0100

    docs: explain Mainsails username registration schema (#274)

--- docs/mainsail/transactions/types/username-registration.md.blade.php
+++ docs/mainsail/transactions/types/username-registration.md.blade.php
@@ -5,3 +5,15 @@ title: Transaction Types - Username Registration
 # Username Registration
 
 A user or organization can register the username to their address. Single wallet can contain only one username. Username can be used to identify the wallet in the network. Username is unique to the address and other wallets cannot use the same username until it is free again. Username transaction can be called multiple time to set or update the username.
+
+## Username schema
+
+Transaction can register **username** with the follwing properties:
+
+- minimum length of 1 character
+- maximum length of 20 characters
+- only lowercase letters, numbers and underscores are allowed
+- cannot start or end with underscore
+- cannot contain two or more consecutive underscores
+
+Check out the [Username Registration Transaction - Schema](https://github.com/ArkEcosystem/mainsail/blob/f831da020d0dd13c441bd2c84b597f9df89a5672/packages/crypto-transaction-username-registration/source/validation/schemas.ts#L4-L7) for implementation.
