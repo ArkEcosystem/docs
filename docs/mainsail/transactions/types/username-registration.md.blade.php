fb867a5b3d2aba787a5161c6364ccd812e923dd2
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-03-06 10:43:25 +0100

    Explain schema

--- docs/mainsail/transactions/types/username-registration.md.blade.php
+++ docs/mainsail/transactions/types/username-registration.md.blade.php
@@ -5,3 +5,12 @@ title: Transaction Types - Username Registration
 # Username Registration
 
 A user or organization can register the username to their address. Single wallet can contain only one username. Username can be used to identify the wallet in the network. Username is unique to the address and other wallets cannot use the same username until it is free again. Username transaction can be called multiple time to set or update the username.
+
+## Username schema
+
+Transaction can register **username** with the follwing properties:
+- minimum length of 1 character
+- maximum length of 20 characters
+- only lowercase letters, numbers and hyphens are allowed
+- cannot start or end with hyphen
+- cannot contain two or more consecutive hyphens
