073fc83b30b59701f457dbceef347ce46b41a5e1
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-03-06 10:51:04 +0100

    Underscores

--- docs/mainsail/transactions/types/username-registration.md.blade.php
+++ docs/mainsail/transactions/types/username-registration.md.blade.php
@@ -12,8 +12,8 @@ Transaction can register **username** with the follwing properties:
 
 - minimum length of 1 character
 - maximum length of 20 characters
-- only lowercase letters, numbers and hyphens are allowed
-- cannot start or end with hyphen
-- cannot contain two or more consecutive hyphens
+- only lowercase letters, numbers and underscores are allowed
+- cannot start or end with underscores
+- cannot contain two or more consecutive underscores
 
 Check out the [Username Registration Transaction - Schema](https://github.com/ArkEcosystem/mainsail/blob/f831da020d0dd13c441bd2c84b597f9df89a5672/packages/crypto-transaction-username-registration/source/validation/schemas.ts#L4-L7) for implementation.
